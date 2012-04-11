8058c6d9b58433fdfc0f727f3f957c2027e61217
jwhong <james.whong@gmail.com>
2012-04-11 04:11:25 +0000

    Added wxpython selection pane to plotter
    git-svn-id: http://projecthexapod.com/svn@59 c308ba4a-1832-4cc7-b836-58aacb0a83c6

---
+++ plotter/PlotFrame.py
@@ -0,0 +1,277 @@
+import wx
+from wx.lib.plot import *
+# Needs Numeric or numarray or NumPy
+try:
+    import numpy.oldnumeric as _Numeric
+except:
+    try:
+        import numarray as _Numeric  #if numarray is used it is renamed Numeric
+    except:
+        try:
+            import Numeric as _Numeric
+        except:
+            msg= """
+            This module requires the Numeric/numarray or NumPy module,
+            which could not be imported.  It probably is not installed
+            (it's not part of the standard Python distribution). See the
+            Numeric Python site (http://numpy.scipy.org) for information on
+            downloading source or binaries."""
+            raise ImportError, "Numeric,numarray or NumPy not found. \n" + msg
+
+class PlotFrame(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title,
+                          wx.DefaultPosition, (600, 400))
+
+        # Now Create the menu bar and items
+        self.mainmenu = wx.MenuBar()
+
+        menu = wx.Menu()
+        menu.Append(200, 'Page Setup...', 'Setup the printer page')
+        self.Bind(wx.EVT_MENU, self.OnFilePageSetup, id=200)
+        
+        menu.Append(201, 'Print Preview...', 'Show the current plot on page')
+        self.Bind(wx.EVT_MENU, self.OnFilePrintPreview, id=201)
+        
+        menu.Append(202, 'Print...', 'Print the current plot')
+        self.Bind(wx.EVT_MENU, self.OnFilePrint, id=202)
+        
+        menu.Append(203, 'Save Plot...', 'Save current plot')
+        self.Bind(wx.EVT_MENU, self.OnSaveFile, id=203)
+        
+        menu.Append(205, 'E&xit', 'Enough of this already!')
+        self.Bind(wx.EVT_MENU, self.OnFileExit, id=205)
+        self.mainmenu.Append(menu, '&File')
+
+        menu = wx.Menu()
+        menu.Append(211, '&Redraw', 'Redraw plots')
+        self.Bind(wx.EVT_MENU,self.OnPlotRedraw, id=211)
+        menu.Append(212, '&Clear', 'Clear canvas')
+        self.Bind(wx.EVT_MENU,self.OnPlotClear, id=212)
+        menu.Append(213, '&Scale', 'Scale canvas')
+        self.Bind(wx.EVT_MENU,self.OnPlotScale, id=213) 
+        menu.Append(214, 'Enable &Zoom', 'Enable Mouse Zoom', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableZoom, id=214) 
+        menu.Append(215, 'Enable &Grid', 'Turn on Grid', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableGrid, id=215)
+        menu.Append(217, 'Enable &Drag', 'Activates dragging mode', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableDrag, id=217)
+        menu.Append(220, 'Enable &Legend', 'Turn on Legend', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableLegend, id=220)
+        menu.Append(222, 'Enable &Point Label', 'Show Closest Point', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnablePointLabel, id=222)
+        
+        menu.Append(223, 'Enable &Anti-Aliasing', 'Smooth output', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableAntiAliasing, id=223)
+        menu.Append(224, 'Enable &High-Resolution AA', 'Draw in higher resolution', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableHiRes, id=224)
+        
+        menu.Append(226, 'Enable Center Lines', 'Draw center lines', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableCenterLines, id=226)
+        menu.Append(227, 'Enable Diagonal Lines', 'Draw diagonal lines', kind=wx.ITEM_CHECK)
+        self.Bind(wx.EVT_MENU,self.OnEnableDiagonals, id=227)
+        
+        menu.Append(231, 'Set Gray Background', 'Change background colour to gray')
+        self.Bind(wx.EVT_MENU,self.OnBackgroundGray, id=231)
+        menu.Append(232, 'Set &White Background', 'Change background colour to white')
+        self.Bind(wx.EVT_MENU,self.OnBackgroundWhite, id=232)
+        menu.Append(233, 'Set Red Label Text', 'Change label text colour to red')
+        self.Bind(wx.EVT_MENU,self.OnForegroundRed, id=233)
+        menu.Append(234, 'Set &Black Label Text', 'Change label text colour to black')
+        self.Bind(wx.EVT_MENU,self.OnForegroundBlack, id=234)
+       
+        menu.Append(225, 'Scroll Up 1', 'Move View Up 1 Unit')
+        self.Bind(wx.EVT_MENU,self.OnScrUp, id=225) 
+        menu.Append(230, 'Scroll Rt 2', 'Move View Right 2 Units')
+        self.Bind(wx.EVT_MENU,self.OnScrRt, id=230)
+        menu.Append(235, '&Plot Reset', 'Reset to original plot')
+        self.Bind(wx.EVT_MENU,self.OnReset, id=235)
+
+        self.mainmenu.Append(menu, '&Plot')
+
+        menu = wx.Menu()
+        menu.Append(300, '&About', 'About this thing...')
+        self.Bind(wx.EVT_MENU, self.OnHelpAbout, id=300)
+        self.mainmenu.Append(menu, '&Help')
+
+        self.SetMenuBar(self.mainmenu)
+
+        # A status bar to tell people what's happening
+        self.CreateStatusBar(1)
+        
+        self.client = PlotCanvas(self)
+        #define the function for drawing pointLabels
+        self.client.SetPointLabelFunc(self.DrawPointLabel)
+        # Create mouse event for showing cursor coords in status bar
+        self.client.canvas.Bind(wx.EVT_LEFT_DOWN, self.OnMouseLeftDown)
+        # Show closest point when enabled
+        self.client.canvas.Bind(wx.EVT_MOTION, self.OnMotion)
+
+        self.Show(True)
+
+    def DrawPointLabel(self, dc, mDataDict):
+        """This is the fuction that defines how the pointLabels are plotted
+            dc - DC that will be passed
+            mDataDict - Dictionary of data that you want to use for the pointLabel
+
+            As an example I have decided I want a box at the curve point
+            with some text information about the curve plotted below.
+            Any wxDC method can be used.
+        """
+        # ----------
+        dc.SetPen(wx.Pen(wx.BLACK))
+        dc.SetBrush(wx.Brush( wx.BLACK, wx.SOLID ) )
+        
+        sx, sy = mDataDict["scaledXY"] #scaled x,y of closest point
+        dc.DrawRectangle( sx-5,sy-5, 10, 10)  #10by10 square centered on point
+        px,py = mDataDict["pointXY"]
+        cNum = mDataDict["curveNum"]
+        pntIn = mDataDict["pIndex"]
+        legend = mDataDict["legend"]
+        #make a string to display
+        s = "Crv# %i, '%s', Pt. (%.2f,%.2f), PtInd %i" %(cNum, legend, px, py, pntIn)
+        dc.DrawText(s, sx , sy+1)
+        # -----------
+
+    def OnMouseLeftDown(self,event):
+        s= "Left Mouse Down at Point: (%.4f, %.4f)" % self.client._getXY(event)
+        self.SetStatusText(s)
+        event.Skip()            #allows plotCanvas OnMouseLeftDown to be called
+
+    def OnMotion(self, event):
+        #show closest point (when enbled)
+        if self.client.GetEnablePointLabel() == True:
+            #make up dict with info for the pointLabel
+            #I've decided to mark the closest point on the closest curve
+            dlst= self.client.GetClosestPoint( self.client._getXY(event), pointScaled= True)
+            if dlst != []:    #returns [] if none
+                curveNum, legend, pIndex, pointXY, scaledXY, distance = dlst
+                #make up dictionary to pass to my user function (see DrawPointLabel) 
+                mDataDict= {"curveNum":curveNum, "legend":legend, "pIndex":pIndex,\
+                            "pointXY":pointXY, "scaledXY":scaledXY}
+                #pass dict to update the pointLabel
+                self.client.UpdatePointLabel(mDataDict)
+        event.Skip()           #go to next handler
+
+    def OnFilePageSetup(self, event):
+        self.client.PageSetup()
+        
+    def OnFilePrintPreview(self, event):
+        self.client.PrintPreview()
+        
+    def OnFilePrint(self, event):
+        self.client.Printout()
+        
+    def OnSaveFile(self, event):
+        self.client.SaveFile()
+
+    def OnFileExit(self, event):
+        self.Close()
+
+    def drawData(self, event):
+        colors = ['red', 'green', 'blue', 'black']
+        markers = []
+        i = 0
+        for k,v in self.data.items():
+            if k == 'time':
+                continue
+            markers.append( PolyMarker(zip(self.data['time'], self.data[k]), legend=k, colour=colors[i], marker='dot',size=1) )
+            i+=1
+        graphics = PlotGraphics(markers,"Graph Title", "X Axis", "Y Axis")
+        self.resetDefaults()
+        self.client.SetXSpec('min')
+        self.client.SetYSpec('min')
+        self.client.Draw(graphics)
+
+    def OnPlotRedraw(self,event):
+        self.client.Redraw()
+
+    def OnPlotClear(self,event):
+        self.client.Clear()
+        
+    def OnPlotScale(self, event):
+        if self.client.last_draw != None:
+            graphics, xAxis, yAxis= self.client.last_draw
+            self.client.Draw(graphics,(1,3.05),(0,1))
+
+    def OnEnableZoom(self, event):
+        self.client.SetEnableZoom(event.IsChecked())
+        self.mainmenu.Check(217, not event.IsChecked())
+        
+    def OnEnableGrid(self, event):
+        self.client.SetEnableGrid(event.IsChecked())
+        
+    def OnEnableDrag(self, event):
+        self.client.SetEnableDrag(event.IsChecked())
+        self.mainmenu.Check(214, not event.IsChecked())
+        
+    def OnEnableLegend(self, event):
+        self.client.SetEnableLegend(event.IsChecked())
+
+    def OnEnablePointLabel(self, event):
+        self.client.SetEnablePointLabel(event.IsChecked())
+
+    def OnEnableAntiAliasing(self, event):
+        self.client.SetEnableAntiAliasing(event.IsChecked())
+
+    def OnEnableHiRes(self, event):
+        self.client.SetEnableHiRes(event.IsChecked())
+
+    def OnEnableCenterLines(self, event):
+        self.client.SetEnableCenterLines(event.IsChecked())
+
+    def OnEnableDiagonals(self, event):
+        self.client.SetEnableDiagonals(event.IsChecked())
+    
+    def OnBackgroundGray(self, event):
+        self.client.SetBackgroundColour("#CCCCCC")
+        self.client.Redraw()
+    
+    def OnBackgroundWhite(self, event):
+        self.client.SetBackgroundColour("white")
+        self.client.Redraw()
+    
+    def OnForegroundRed(self, event):
+        self.client.SetForegroundColour("red")
+        self.client.Redraw()
+
+    def OnForegroundBlack(self, event):
+        self.client.SetForegroundColour("black")
+        self.client.Redraw()
+
+    def OnScrUp(self, event):
+        self.client.ScrollUp(1)
+        
+    def OnScrRt(self,event):
+        self.client.ScrollRight(2)
+
+    def OnReset(self,event):
+        self.client.Reset()
+
+    def OnHelpAbout(self, event):
+        from wx.lib.dialogs import ScrolledMessageDialog
+        about = ScrolledMessageDialog(self, __doc__, "About...")
+        about.ShowModal()
+
+    def resetDefaults(self):
+        """Just to reset the fonts back to the PlotCanvas defaults"""
+        self.client.SetFont(wx.Font(10,wx.SWISS,wx.NORMAL,wx.NORMAL))
+        self.client.SetFontSizeAxis(10)
+        self.client.SetFontSizeLegend(7)
+        self.client.setLogScale((False,False))
+        self.client.SetXSpec('auto')
+        self.client.SetYSpec('auto')
+
+def __test():
+    class MyApp(wx.App):
+        def OnInit(self):
+            wx.InitAllImageHandlers()
+            frame = PlotFrame(None, -1, "PlotCanvas")
+            #frame.Show(True)
+            self.SetTopWindow(frame)
+            return True
+    app = MyApp(0)
+    app.MainLoop()
+
+if __name__ == '__main__':
+    __test()
---
+++ plotter/SelectFrame.py
@@ -0,0 +1,181 @@
+import wx
+from wx.lib.plot import *
+# Needs Numeric or numarray or NumPy
+try:
+    import numpy.oldnumeric as _Numeric
+except:
+    try:
+        import numarray as _Numeric  #if numarray is used it is renamed Numeric
+    except:
+        try:
+            import Numeric as _Numeric
+        except:
+            msg= """
+            This module requires the Numeric/numarray or NumPy module,
+            which could not be imported.  It probably is not installed
+            (it's not part of the standard Python distribution). See the
+            Numeric Python site (http://numpy.scipy.org) for information on
+            downloading source or binaries."""
+            raise ImportError, "Numeric,numarray or NumPy not found. \n" + msg
+
+sys.path.append('..')
+from SimulationKit.pubsub import *
+from PlotFrame import *
+import time
+import threading
+
+class SelectFrame(wx.Frame):
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title,
+                          wx.DefaultPosition, (600, 400))
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+
+        # Now Create the menu bar and items
+        self.mainmenu = wx.MenuBar()
+
+        menu = wx.Menu()
+        menu.Append(300, '&About', 'About this thing...')
+        self.Bind(wx.EVT_MENU, self.OnHelpAbout, id=300)
+        self.mainmenu.Append(menu, '&Help')
+
+        self.SetMenuBar(self.mainmenu)
+
+        # A status bar to tell people what's happening
+        self.CreateStatusBar(1)
+        
+        # FIXME:  Put this elsewhere
+        self.subscriber    = Subscriber( 'localhost', 5055 )
+
+        self.subscriptions = {}
+        self.data = {}
+
+        #self.animation_callbacks = []
+        #animation_clock_thread = threading.Thread()
+        #animation_clock_thread.run = self.animationClock
+        #animation_clock_thread.setDaemon(True)
+        #animation_clock_thread.start()
+
+        # For each entry in the catalog we must figure out where it belongs in the tree
+        root_d={}
+        for full_name in self.subscriber.catalog:
+            tokens = full_name.split('.')
+            d = root_d
+            for token in tokens:
+                if not d.has_key(token):
+                    d[token] = {}
+                d = d[token]
+
+        # The outermost sizer
+        self.vsizer_0 = wx.BoxSizer(wx.VERTICAL)
+
+        # list of available variables
+        self.catalog = wx.TreeCtrl(self, wx.NewId(), style=wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS|wx.TR_FULL_ROW_HIGHLIGHT|wx.TR_ROW_LINES)
+        root_item = self.catalog.AddRoot('root')
+        def walkDictForTree(d,tree_item):
+            for k,v in d.items():
+                child_item = self.catalog.AppendItem(tree_item, k)
+                walkDictForTree( v, child_item )
+            self.catalog.SortChildren(tree_item)
+        walkDictForTree( root_d, root_item )
+        self.Bind( wx.EVT_TREE_ITEM_ACTIVATED, self.OnCatalogItemActivated, self.catalog )
+
+        self.play_stop_button = wx.Button( self, -1, "Play" )
+        self.Bind( wx.EVT_BUTTON, self.OnPlayStopButton, self.play_stop_button )
+
+        self.vsizer_0.Add( self.catalog          , 1 , wx.EXPAND , 0 );
+        self.vsizer_0.Add( self.play_stop_button , 0 , wx.EXPAND , 0 );
+        self.SetSizer(self.vsizer_0)
+
+        self.Show(True)
+    def OnCatalogItemActivated( self, event ):
+        itemid = event.GetId()
+        tree = event.GetEventObject()
+        treeitem = tree.GetSelection()
+        text = tree.GetItemText(treeitem)
+        # If this item has children keep propagating
+        if tree.ItemHasChildren( treeitem ):
+            if tree.IsExpanded( treeitem ):
+                tree.Collapse( treeitem )
+            else:
+                tree.Expand( treeitem )
+            return
+        else:
+            full_name = ''
+            item = treeitem
+            root = tree.GetRootItem()
+            while item != root:
+                full_name = tree.GetItemText(item) + '.' + full_name
+                item = tree.GetItemParent( item )
+            full_name = full_name[:-1]
+            if self.subscriptions.has_key(full_name):
+                tree.SetItemBackgroundColour( treeitem, (255,255,255) )
+                del self.subscriptions[full_name]
+                # Recurse up the tree, change colors appropriately
+                item = tree.GetItemParent( treeitem )
+                while item != root:
+                    # Check the active leaf count
+                    count = tree.GetItemPyData( item )
+                    if count:
+                        count -= 1
+                    tree.SetItemPyData( item, count )
+                    if not count:
+                        tree.SetItemBackgroundColour( item, (255,255,255) )
+                    item  = tree.GetItemParent( item )
+            else:
+                tree.SetItemBackgroundColour( treeitem, (100,255,100) )
+                self.subscriptions[full_name] = 0
+                # Recurse up the tree, change colors appropriately
+                item = tree.GetItemParent( treeitem )
+                while item != root:
+                    # Check the active leaf count
+                    count = tree.GetItemPyData( item )
+                    if count == None:
+                        count = 0
+                    count += 1
+                    tree.SetItemPyData( item, count )
+                    tree.SetItemBackgroundColour( item, (255,255,100) )
+                    item  = tree.GetItemParent( item )
+    def OnDataFrameReceived( self, frame ):
+        for k,v in frame.items():
+            self.data[k].pop(0)
+            self.data[k].append(v)
+    def OnPlayStopButton(self, event):
+        if self.play_stop_button.GetLabel() == 'Play':
+            self.play_stop_button.SetLabel('Stop')
+            self.data = { k:[0 for x in range(1000)] for k in self.subscriptions }
+            self.subscriber.subscribeTo( self.subscriptions )
+            self.subscriber.setCallback( self.OnDataFrameReceived )
+            self.subscriber.start()
+            if hasattr(self, 'plot_frame'):
+                self.plot_frame.Destroy()
+            self.plot_frame = PlotFrame(self, -1, "PlotCanvas")
+            self.plot_frame.data = self.data
+            self.animation_timer = wx.Timer(self)
+            self.Bind(wx.EVT_TIMER, self.plot_frame.drawData, self.animation_timer)
+            self.animation_timer.Start(100,0)
+        else:
+            self.animation_timer.Stop()
+            self.play_stop_button.SetLabel('Play')
+            self.subscriber.close()
+            self.subscriber    = Subscriber( 'localhost', 5055 )
+    def OnClose(self, event):
+        self.subscriber.close()
+        self.Destroy()
+    def OnHelpAbout(self, event):
+        from wx.lib.dialogs import ScrolledMessageDialog
+        about = ScrolledMessageDialog(self, __doc__, "About...")
+        about.ShowModal()
+
+def __test():
+    class MyApp(wx.App):
+        def OnInit(self):
+            #wx.InitAllImageHandlers()
+            frame = SelectFrame(None, -1, "Selection Panel")
+            #frame.Show(True)
+            self.SetTopWindow(frame)
+            return True
+    app = MyApp(0)
+    app.MainLoop()
+
+if __name__ == '__main__':
+    __test()
