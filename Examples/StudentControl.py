2e2aed29eee53adad549dfc2021123b61cf9a4e3
jwhong <james.whong@gmail.com>
2012-04-20 05:46:06 +0000

    Reverted kick_test and StudentControl... I did not mean to commit a solution to the bowling problem.  My mistake.
    git-svn-id: http://projecthexapod.com/svn@88 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Examples/StudentControl.py
+++ Examples/StudentControl.py
@@ -1,23 +1,7 @@
-from math import pi
-
-def sign(i):
-    if i >= 0:
-        return 1
-    return -1
 
 def control( sim_time, hip_yaw_angle, hip_pitch_angle, knee_angle ):
     ### YOUR CODE GOES HERE ###
-    if sim_time < 3:
-        hip_yaw_lrate   = 0.00
-        hip_pitch_lrate = 0.25*(-pi/8-hip_pitch_angle)
-        knee_lrate      = 0.25*(2*pi/3-knee_angle)
-    elif sim_time < 5:
-        hip_yaw_lrate   = 0.25*(0-hip_yaw_angle)
-        hip_pitch_lrate = 0.00
-        knee_lrate      = 0.00
-    else:
-        hip_yaw_lrate   = 0.00
-        hip_pitch_lrate = 0.00
-        knee_lrate      = 0.2*(0-knee_angle)
-        
+    hip_yaw_lrate   = 0.01
+    hip_pitch_lrate = 0.00
+    knee_lrate      = 0.00
     return (hip_yaw_lrate, hip_pitch_lrate, knee_lrate)
