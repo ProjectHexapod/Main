9b8de46cbb2057dbc5ad23c6e8ef55503952af1d
jwhong <james.whong@gmail.com>
2012-04-25 07:02:41 +0000

    added bowling pins to the barge demo, for funsies.
    git-svn-id: http://projecthexapod.com/svn@96 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Examples/barge.py
+++ Examples/barge.py
@@ -8,7 +8,21 @@ import time
 from math import *
 
 d = {'offset':(0,0,0.67)}
-s = Simulator(dt=1e-4,plane=1,pave=0,graphical=1,robot=LegOnStand,robot_kwargs=d, start_paused = True)
+s = Simulator(dt=1e-3,plane=1,pave=0,graphical=1,robot=LegOnStand,robot_kwargs=d, start_paused = True)
+
+# Put up the goal posts
+wickets = []
+static_geoms = []
+row_n = 6
+x_off = .5
+y_off = .5
+i = 0
+# Rack up the pins
+for i in range(row_n):
+    for j in range(i+1):
+        body, geom = s.createCapsule( mass = 1.0e1, length = 1.0, radius = 0.1, pos = (i*x_off, -5.0 - y_off*j + i*(y_off/2),0.7) )
+        wickets.append(body)
+        static_geoms.append(geom)
 
 yaw   = s.robot.members['hip_yaw']
 pitch = s.robot.members['hip_pitch']
