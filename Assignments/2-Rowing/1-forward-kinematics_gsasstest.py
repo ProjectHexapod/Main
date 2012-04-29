45749a9ba2e848b4ea49c8403e73614c5ef5d8a4
gsass <george.a.sass@gmail.com>
2012-04-29 23:29:24 +0000

    Some changes to George's FK test code.
    Changed over transforms to homogeneous tranforms, fixed bug where actual return values were being lost in the ether.
    This is just my test code, which is why it's named something different, and I make no promises that it will work.
    
    
    
    git-svn-id: http://projecthexapod.com/svn@101 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/2-Rowing/1-forward-kinematics_gsasstest.py
+++ Assignments/2-Rowing/1-forward-kinematics_gsasstest.py
@@ -1,5 +1,5 @@
 from math import pi, sin, cos
-from numpy import matrix
+from numpy import matrix, transpose, resize
 
 def jointAnglesFromFootPosition( hip_yaw_angle, hip_pitch_angle, knee_angle, robot ):
     # YOUR ASSIGNMENT: 
@@ -25,41 +25,92 @@ def jointAnglesFromFootPosition( hip_yaw_angle, hip_pitch_angle, knee_angle, rob
     thighVector=(robot.THIGH_L,0,0);
     calfVector(robot.CALF_L,0,0);
     
-    kneePos=pitchRotation(calfVector,knee_angle);
-    hipPos=pitchRotation(thighVector+kneePos,hip_pitch_angle);
-    footpos=-yawRotation(yawVector+hipPos,hip_yaw_angle);
+    kneePos=pitchRotation(calfVector,knee_angle,matrix[0,0,0]);
+    hipPos=pitchRotation(thighVector,hip_pitch_angle,kneePos);
+    #Negated because we've been calculating the vector from foot to robot
+    footpos=-yawRotation(yawVector,hip_yaw_angle,hipPos);
+    
+    foot_x=footpos[0,0];
+    foot_y=footpos[1,0];
+    foot_z=footpos[2,0];
     return (foot_x, foot_y, foot_z);
 
-def yawRotation(vector, angle):
+def yawRotation(vector, angle, offset):
 	#Rotates a vector around the Z axis by an angle (uses radians)
-	row1=[cos(angle),-sin(angle),0];
-	row2=[sin(angle),cos(angle),0];
-	row3=[0,0,1];
-	rotationMatrix=matrix([row1,row2,row3]);ch 
-	try:
-		posVector=vector*rotationMatrix;
-	except ValueError:
-		posVector=transpose(vector)*rotationMatrix;
-	return (posVector)
-def pitchRotation(vector, angle):
+	offset=toRowVector(offset);
+	
+	row1=[cos(angle),-sin(angle),0,offset[0,0]];
+	row2=[sin(angle),cos(angle),0,offset[0,1]];
+	row3=[0,0,1],offset[0,2];
+	row4=[0,0,0,1];
+	rotationMatrix=matrix([row1,row2,row3,row4]);
+	
+	homogenous=rotationMatrix*toHomogenous(vector);
+	posVector=homogenous[0:3,0];
+	return (posVector);
+
+def pitchRotation(vector, angle, offset):
 	#Rotates a vector around the Y axis by an angle (uses radians)
-	row1=[cos(angle),0,sin(angle)];
-	row2=[0,1,0];
-	row3=[-sin(angle),0,cos(angle)];
-	rotationMatrix=matrix([row1,row2,row3]);
-	try:
-		posVector=vector*rotationMatrix;
-	except ValueError:
-		posVector=transpose(vector)*rotationMatrix;
+	offset=toRowVector(offset);
+	
+	row1=[cos(angle),0,sin(angle),offset[0,0]];
+	row2=[0,1,0,offset[0,1]];
+	row3=[-sin(angle),0,cos(angle),offset[0,2]];
+	row4=[0,0,0,1]
+	rotationMatrix=matrix([row1,row2,row3,row4]);
+	
+	homogenous=rotationMatrix*toHomogenous(vector);
+	posVector=homogenous[0:3,0];
 	return (posVector)
+
 def rollRotation(vector, angle):
 	#Rotates a vector around the X axis by an angle (uses radians)
-	row1=[1,0,0];
-	row2=[0,cos(angle),-sin(angle)];
-	row3=[0,sin(angle),cos(angle)];
-	rotationMatrix=matrix([row1,row2,row3]);
-	try:
-		posVector=vector*rotationMatrix;
-	except ValueError:
-		posVector=transpose(vector)*rotationMatrix;
-	return (posVector)
+	offset=toRowVector(offset);
+	
+	row1=[1,0,0,offset[0,0]];
+	row2=[0,cos(angle),-sin(angle),offset[0,1]];
+	row3=[0,sin(angle),cos(angle),offset[0,2]];
+	row4=[0,0,0,1];
+	rotationMatrix=matrix([row1,row2,row3,row4]);
+	
+	homogenous=rotationMatrix*toHomogenous(vector);
+	posVector=homogenous[0:3,0];
+	return (posVector);
+
+def Translation(vector, offset):
+	#Performs a purely translational transform on a vector
+	offset=toColVector(offset);
+	
+	row1=[1,0,0,offset[0,0]];
+	row2=[0,1,0,offset[1,0]];
+	row3=[0,0,1,offset[2,0]];
+	row4=[0,0,0,1];
+	homogenous=rotationMatrix*toHomogenous(vector);
+	posVector=homogenous[0:3,0];
+	return (posVector);
+
+def toColVector(vector):
+	if vector.shape[1]!=1:
+		colVector=transpose(vector);
+	else:
+		colVector=vector;
+	if colVector.shape[1]!=1:
+		raise ValueError("Vector is not 1-dimensional,"+
+		" impossible to convert to column vector.")
+	return colVector;
+
+def toRowVector(vector):
+	if vector.shape[0]!=1:
+		rowVector=transpose(vector);
+	else:
+		colVector=vector;
+	if rowVector.shape[0]!=1:
+		raise ValueError("Vector is not 1-dimensional,"+
+		" impossible to convert to row vector.")
+	return rowVector;
+
+def toHomogenous(vector):
+	vector=toColVector(vector);
+	homogenous=resize(vector,[4,1]);
+	homogenous[3,0]=1
+	return homogenous;
