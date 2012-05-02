19a2f752874799882ed71f0e45b1cb6dee774631
gsass <george.a.sass@gmail.com>
2012-05-02 02:07:56 +0000

    Inverse kinematics written.
    Some notes and caveats:
      1. We calculate the magnitude of the projection of the leg onto the ground using pythagorean theorem, instead of performing a vector transform and finding the magnitude of that vector.
      2. Our helper function for law of cosines returns the angle opposite the last side passed in.  Heyo!  If you don't know about the law of cosines this would not make a ton of sense so I'm going to change that to something more human understandable.
      3. Haha comments aren't really in there yet; let me add those immediately right now.
    
    
    
    git-svn-id: http://projecthexapod.com/svn@106 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/2-Rowing/2-inverse-kinematics.py
+++ Assignments/2-Rowing/2-inverse-kinematics.py
@@ -1,4 +1,8 @@
-from math import pi
+from math import atan2, acos, sqrt
+
+def __main__:
+    footPositionFromJointAngles(
+    return
 
 def footPositionFromJointAngles( foot_x, foot_y, foot_z, robot ):
     # YOUR ASSIGNMENT: 
@@ -19,5 +23,26 @@ def footPositionFromJointAngles( foot_x, foot_y, foot_z, robot ):
     # Positive hip pitch and knee pitch curl the leg under the robot
 
     ### YOUR CODE GOES HERE ###
-    return (hip_yaw_angle, hip_pitch_angle, knee_angle)
     
+    hip_yaw_angle=atan2(foot_x,foot_y);
+    foot_ground_projection_magnitude=sqrt(foot_x**2+foot_y**2);
+    
+    #imaginary magnitude between foot and hip for law of cosines
+    #based math
+    hip_to_foot_magnitude=sqrt(
+    (foot_ground_projection_magnitude-robot.YAW_L)**2+foot_z**2);
+    
+    #deriving helper angle between projected x axis and foot->hip vector
+    a1=atan2(foot_z,foot_ground_projection_magnitude);
+	#deriving helper angle between thigh and foot->hip vector
+    a2=law_of_cosines(robot.THIGH_L,hip_to_foot_magnitude,robot.CALF_L);
+    hip_pitch_angle=a1+a2;
+    
+    knee_angle=law_of_cosines(robot.THIGH_L,robot.CALF_L,
+	hip_to_foot_magnitude);
+    
+    return (hip_yaw_angle, hip_pitch_angle, knee_angle);
+   
+def law_of_cosines(a,b,c):
+	angle=acos((a**2+b**2-c**2)/2*a*b);
+	return angle;
