dee90ac70f6e7f091f1620b7c55945985b213e2e
becomesaflame <spark.scul@gmail.com>
2012-05-07 05:13:26 +0000

    fixed conflict
    git-svn-id: http://projecthexapod.com/svn@124 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/2-Rowing/joint_control.py
+++ Assignments/2-Rowing/joint_control.py
@@ -4,9 +4,10 @@
 from math import pi
 
 class JointController:
+
     def __init__(self, joint):
         self.prev_error = 0  # error in radians at the time the function is called
-        self.prev_time = -1  # system time when the function was last called
+        self.prev_time = 0  # system time when the function was last called
         self.iterm = 0
         self.valve_pos = 0
         self.outMin = -200   # minimum allowable flow rate 
@@ -14,38 +15,25 @@ class JointController:
         self.joint = joint   # make joint into a global variable
 
         # set weights
-        self.kp = 0.20
-        self.ki = 0.20
-        self.kd = 0.20
+        self.kp = 1
+        self.ki = 1
+        self.kd = 1
 
-    # sim_time: current system time in seconds
-    # target_angle: target angle for joint in degrees 
-    # joint:
     # sys_time: current system time in seconds
-    def updateJoint(self, target_angle, sys_time):
-        # YOUR ASSIGNMENT: 
-        #         # update the joint with the calculated flow rate
-        # Given the target angle on the joint, command a sane linear velocity
-        # to make the joint hit the target angle.
-        # You should think about not introducing sudden movements in to the system
-        # 
-        # All lengths are in meters, all angles in radians
-        #
-        # Robot link lengths are in:
-        # robot.YAW_L  
-        # robot.THIGH_L
-        # robot.CALF_L
-        # Joint angles are positive in the direction of cylinder expansions.
-        # Looking from above, positive hip yaw swings the leg clockwise
-        # Positive hip pitch and knee pitch curl the leg under the robot
+    # target_angle: target angle for joint in degrees 
 
-	        
+
+    def updateJoint(self, target_angle, sys_time):
+        #check that target angle is within bounds
+        if abs(target_angle) > 2*pi:
+            raise ValueError("Invalid target angle") 
+
         cur_error = target_angle - self.joint.getAngle()
 	        
         dtime = sys_time - self.prev_time
 
         #if no time has passed since last function call return last valve_pos
-        if dtime == 0 or self.prev_time < 0:
+        if dtime == 0:
             return self.valve_pos
 
         #calculate P term
@@ -65,6 +53,7 @@ class JointController:
 
         # calculate valve position output
         self.valve_pos = pterm + self.iterm + dterm
+
         # clamp outpout at maximum/minimum output
         if self.valve_pos > self.outMax: 
             self.valve_pos = self.outMax
@@ -72,7 +61,7 @@ class JointController:
             self.valve_pos = self.outMin
 
         self.prev_error = cur_error
-        self.prev_time = sim_time
+        self.prev_time = sys_time
 
         # update the joint with the calculated flow rate
         self.outputFlowRate()
@@ -82,5 +71,3 @@ class JointController:
     # update the joint with the calculated flow rate
     def outputFlowRate(self):
         self.joint.setLengthRate(self.valve_pos)
-
-
