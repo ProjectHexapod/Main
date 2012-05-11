0e9f599471a79234f8d84ad867301ead035872bb
dydtc <dtydc5@gmail.com>
2012-05-11 13:00:12 +0000

    Fixed unittest setup. Run "python run_tests.py" to run all tests.
    Removed the tests/ directory. It's convenient to be in the same directory as the code you're importing.
    
    run_tests.py will find any file named "test*.py" and will run the tests it contains. A "test" is defined as any method that is named like "test*(self)" in a class that inherits from unittest.TestCase.
    
    
    git-svn-id: http://projecthexapod.com/svn@127 c308ba4a-1832-4cc7-b836-58aacb0a83c6

---
+++ Assignments/4-Refactoring/run_tests.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+
+
+def load_tests(loader, standard_tests, pattern):
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    package_tests = loader.discover(this_dir)
+    standard_tests.addTests(package_tests)
+    return standard_tests
+
+unittest.main()
---
+++ Assignments/4-Refactoring/test_leg_controller.py
@@ -0,0 +1,9 @@
+import unittest
+from leg_controller import LegController
+
+class LegControllerTestCase(unittest.TestCase):
+    def testTheTestingFramework(self):
+        self.assertFalse(True)
+
+if __name__ == '__main__':
+    unittest.main()
