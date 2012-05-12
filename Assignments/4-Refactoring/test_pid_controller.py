0bf0875c8c463440eac8ca97acfcd9ba5cf90762
mattdunlap <mattdunlap@gmail.com>
2012-05-12 02:42:44 +0000

    preliminary port of old unit tests
    git-svn-id: http://projecthexapod.com/svn@131 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/test_pid_controller.py
+++ Assignments/4-Refactoring/test_pid_controller.py
@@ -5,9 +5,40 @@ from pid_controller import PidController
 
 class PidControllerTestCase(unittest.TestCase):
     def setUp(self):
-        self.pid = PidController(0.0, 0.0, 0.0)
+        self.pid = PidController(0.2, 0.02, 0.1)
     def tearDown(self):
         pass
+
+    def testUpdateNoop(self):
+        self.assertEquals(0, self.pid.update(.1, 0, 0))
+
+    def testUpdateSmallPositiveError(self):
+        self.assertTrue(0 < self.pid.update(.1, .1, 0))
+
+    def testUpdateSmallNegativeError(self):
+        self.assertTrue(0 > self.pid.update(.1, -.1, 0))
+
+    def testUpdatePTerm(self):
+        self.pid.ki = 0
+        self.pid.kd = 0
+        small = self.pid.update(.1, .1, 0)
+        large = self.pid.update(.1, 2, 0)
+        self.assertTrue(small < large)
+
+    def testUpdateDTerm(self):
+        self.pid.kp = 0
+        self.pid.ki = 0
+        large = self.pid.update(.1, .1, 0)
+        self.pid.error_1 = .2
+        small = self.pid.update(.1, .1, 0)
+        self.assertTrue(small < large)
+
+    def testUpdateITerm(self):
+        self.pid.kp = 0
+        self.pid.kd = 0
+        small = self.pid.update(.1, .1, 0)
+        large = self.pid.update(.1, .1, 0)
+        self.assertTrue(small < large)
 
 
 if __name__ == '__main__':
