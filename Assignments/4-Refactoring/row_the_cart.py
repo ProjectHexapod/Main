cda022e3363e5bc78c4561af936e68f7fb3d7f9c
dydtc <dtydc5@gmail.com>
2012-05-12 22:48:13 +0000

    Hacked on row_the_cart.py to play with PutFootOnGround behavior.
    git-svn-id: http://projecthexapod.com/svn@155 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/row_the_cart.py
+++ Assignments/4-Refactoring/row_the_cart.py
@@ -1,3 +1,4 @@
+from math_utils import *
 from leg_controller import LegController
 import behaviors
 
@@ -5,23 +6,35 @@ import behaviors
 # Initialization
 leg = LegController()
 state = 0
-traj = behaviors.PutFootOnGround(leg, 0.05)
+traj = None
 
 
 # Body of control loop
 def update(time, yaw, hip_pitch, knee_pitch, shock_depth):
-    # Monitor trajectories
-    if traj.isDone():
-        # traj = behaviors.?
-        pass
-
+    global state, traj
+    
+    
     # Update leg
     leg.setTime(time)
     leg.setLegState(yaw, hip_pitch, knee_pitch, shock_depth)
     leg.updateFootOnGround()
 
+    # Monitor trajectories
+    if state == 1:
+        state = 2
+        traj = behaviors.PutFootOnGround(leg, 0.1)
+        # traj = behaviors.?
+        pass
+
     # Evaluate trajectory and joint control
-    leg.setDesiredJointAngles(traj.update(time))
+    if time < 2.0:
+        leg.setDesiredJointAngles(array([0.0, -0.5, 0.1 + pi_2]))
+    elif state == 0:
+        state = 1
+    if traj is not None:
+        if leg.isFootOnGround():
+            print leg.getLegState(), traj.isDone()
+        leg.setDesiredJointAngles(traj.update(time, leg.getDeltaTime()))
     leg.updateLengthRateCommands()
 
     # Send commands
