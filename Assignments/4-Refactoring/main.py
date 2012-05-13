a3127d33b835caefa31303ad2c8928d80abe05b7
mycroftxxx <root@ihack.net>
2012-05-13 18:42:19 +0000

    While throwing an exception, stop all the joints.
    git-svn-id: http://projecthexapod.com/svn@166 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/main.py
+++ Assignments/4-Refactoring/main.py
@@ -29,14 +29,21 @@ knee_joint = s.robot.members['knee_pitch']
 shock_joint = s.robot.members['foot_shock']
 
 time_1 = 0.0
-while True:
-    s.step()
+try:
+    while True:
+        s.step()
 
-    time = s.getSimTime()
-    if time != time_1:
-        lr = update(time, yaw_joint.getAngle(), pitch_joint.getAngle(), knee_joint.getAngle(), shock_joint.getPosition())
-        yaw_joint.setLengthRate(lr[0])
-        pitch_joint.setLengthRate(lr[1])
-        knee_joint.setLengthRate(lr[2])
+        time = s.getSimTime()
+        if time != time_1:
+            lr = update(time, yaw_joint.getAngle(), pitch_joint.getAngle(), knee_joint.getAngle(), shock_joint.getPosition())
+            yaw_joint.setLengthRate(lr[0])
+            pitch_joint.setLengthRate(lr[1])
+            knee_joint.setLengthRate(lr[2])
         
-        time_1 = time
+            time_1 = time
+
+except:
+    yaw_joint.setLengthRate(0)
+    pitch_joint.setLengthRate(0)
+    knee_joint.setLengthRate(0)
+    raise
