76f2c68ece7731be9117a0357d0815ea13c22e36
dydtc <dtydc5@gmail.com>
2012-05-13 17:21:37 +0000

    Cleaned up row_the_cart.py a little.
    git-svn-id: http://projecthexapod.com/svn@159 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/row_the_cart.py
+++ Assignments/4-Refactoring/row_the_cart.py
@@ -1,29 +1,19 @@
 from math_utils import *
 from leg_controller import LegController
-import behaviors
+from behaviors import PutFootOnGround, TrapezoidalFootMove
 
 
 # Initialization
 leg = LegController()
 traj = None
 
-S_INIT_MOVE1 = 0
-S_MOVE1 = 1
-S_INIT_LOWER = 2
-S_LOWER = 3
-S_INIT_MOVE2 = 4
-S_MOVE2 = 5
-S_INIT_MOVE3 = 6
-S_MOVE3 = 7
+S_MOVE1 = 0
+S_LOWER = 1
+S_MOVE2 = 2
+S_MOVE3 = 3
 
-state = S_INIT_MOVE1
+state = S_MOVE3
 
-
-def waitFor(traj):
-    if traj.isDone():
-        return 1
-    else:
-        return 0
 
 # Body of control loop
 def update(time, yaw, hip_pitch, knee_pitch, shock_depth):
@@ -36,34 +26,26 @@ def update(time, yaw, hip_pitch, knee_pitch, shock_depth):
     leg.updateFootOnGround()
 
     # Monitor trajectories
-    if state == S_INIT_MOVE1:
-        traj = behaviors.TrapezoidalFootMove(leg,
-                                             array([1.5, -0.6, -0.4]),
-                                             0.2, 0.1)
-        state = S_MOVE1
-    elif state == S_MOVE1:
-        state += waitFor(traj)
-    elif state == S_INIT_LOWER:
-        traj = behaviors.PutFootOnGround(leg, 0.05)
-        state = S_LOWER
-    elif state == S_LOWER:
-        state += waitFor(traj)
-    elif state == S_INIT_MOVE2:
-        traj = behaviors.TrapezoidalFootMove(leg,
-                                             array([1.5, 0.6, leg.getFootPos()[Z]]),
-                                             0.2, 0.1)
-        state = S_MOVE2
-    elif state == S_MOVE2:
-        state += waitFor(traj)
-    elif state == S_INIT_MOVE3:
-        traj = behaviors.TrapezoidalFootMove(leg,
-                                             array([1.5, 0.6, -0.4]),
-                                             0.2, 0.1)
-        state = S_MOVE3
-    elif state == S_MOVE3:
-        state += waitFor(traj)
-    else:
-        state = S_INIT_MOVE1
+    if traj is None or traj.isDone():
+        if state == S_MOVE3:
+            traj = TrapezoidalFootMove(leg,
+                                       array([1.5, -0.6, -0.4]),
+                                       0.2, 0.1)
+            state = S_MOVE1
+        elif state == S_MOVE1:
+            traj = PutFootOnGround(leg, 0.05)
+            state = S_LOWER
+        elif state == S_LOWER:
+            ground_level = leg.getFootPos()[Z]
+            traj = TrapezoidalFootMove(leg,
+                                       array([1.5, 0.6, ground_level]),
+                                       0.2, 0.1)
+            state = S_MOVE2
+        elif state == S_MOVE2:
+            traj = TrapezoidalFootMove(leg,
+                                       array([1.5, 0.6, -0.4]),
+                                       0.2, 0.1)
+            state = S_MOVE3
 
     # Evaluate trajectory and joint control
     leg.setDesiredJointAngles(traj.update(time, leg.getDeltaTime()))
