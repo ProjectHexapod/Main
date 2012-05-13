2de5289742513be45dbda838480f7c5e8b055af9
mattdunlap <mattdunlap@gmail.com>
2012-05-13 18:17:06 +0000

    pid controller variable renames and prep for preventing delta_time div 0
    git-svn-id: http://projecthexapod.com/svn@161 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/pid_controller.py
+++ Assignments/4-Refactoring/pid_controller.py
@@ -4,16 +4,17 @@ class PidController:
         self.ki = ki
         self.kd = kd
 
-        self.error_1 = 0.0
-        self.integral_error_ki = 0.0
+        self.prev_error = 0.0
+        self.prev_response = 0.0
+        self.integral_error_accumulator = 0.0
 
     def update(self, delta_time, desired_pos, measured_pos):
         error = desired_pos - measured_pos
 
-        self.integral_error_ki += self.ki*error * delta_time
-        derivative_error = (error - self.error_1) / delta_time
+        self.integral_error_accumulator += self.ki * error * delta_time
+        derivative_error = (error - self.prev_error) / delta_time
 
-        self.error_1 = error
-
-        return self.kp*error + self.integral_error_ki + self.kd*derivative_error
+        self.prev_error = error
+        self.prev_response = self.kp * error + self.integral_error_accumulator + self.kd * derivative_error
+        return self.prev_response
 
