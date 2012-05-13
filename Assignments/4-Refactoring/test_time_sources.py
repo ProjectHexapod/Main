c652c5196e2d22d00d479dc03b0f872c3b00c39c
dydtc <dtydc5@gmail.com>
2012-05-13 21:46:14 +0000

    Added StopWatch to time_sources.py. It tracks global_time, but always starts counting from t = 0. For example, it's useful for keeping track of the time since a behavior became active.
    git-svn-id: http://projecthexapod.com/svn@176 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/test_time_sources.py
+++ Assignments/4-Refactoring/test_time_sources.py
@@ -1,6 +1,12 @@
 import unittest
 
-from time_sources import TimeSource
+from time_sources import TimeSource, StopWatch
+
+
+# Side effect: tests getTime() and getDelta()
+def check(test_case, ts, time, delta):
+    test_case.assertAlmostEqual(time, ts.getTime())
+    test_case.assertAlmostEqual(delta, ts.getDelta())
 
 
 class TimeSourceTestCase(unittest.TestCase):
@@ -9,37 +15,51 @@ class TimeSourceTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # Side effect: tests getTime() and getDelta()
-    def check(self, time, delta, ts=None):
-        if ts is None:
-            ts = self.ts
-        self.assertEqual(time, ts.getTime())
-        self.assertEqual(delta, ts.getDelta())
-    
     def testDefaultsToZeros(self):
-        self.check(0.0, 0.0)
+        check(self, self.ts, 0.0, 0.0)
     
     def testInitialTime(self):
         ts = TimeSource(1.234, 0.036)
-        self.check(1.234, 0.036, ts)
+        check(self, ts, 1.234, 0.036)
     
     def testUpdateTime(self):
         self.ts.updateTime(0.1)
-        self.check(0.1, 0.1)
+        check(self, self.ts, 0.1, 0.1)
         self.ts.updateTime(0.33)
-        self.check(0.33, 0.23)
+        check(self, self.ts, 0.33, 0.23)
     def testUpdateTimeAlwaysIncreasesTime(self):
         self.assertRaises(ValueError, self.ts.updateTime, 0.0)
         self.assertRaises(ValueError, self.ts.updateTime, -0.1)
         
     def testUpdateDelta(self):
         self.ts.updateDelta(0.1)
-        self.check(0.1, 0.1)
+        check(self, self.ts, 0.1, 0.1)
         self.ts.updateDelta(0.45)
-        self.check(0.55, 0.45)
+        check(self, self.ts, 0.55, 0.45)
     def testUpdateDeltaAlwaysIncreasesTime(self):
         self.assertRaises(ValueError, self.ts.updateDelta, 0.0)
         self.assertRaises(ValueError, self.ts.updateDelta, -0.1)
+
+
+class StopWatchTestCase(unittest.TestCase):
+    def setUp(self):
+        self.ts = TimeSource()
+        self.ts.updateTime(6.0)
+        self.ts.updateDelta(0.1)
+        self.lts = StopWatch(self.ts)
+    def tearDown(self):
+        pass
+    
+    def testStartsCountingFromZero(self):
+        check(self, self.lts, 0.0, 0.1)
+    
+    def testUpdatesLocalTime(self):
+        self.lts.getTime()  # Cause an update of cached data
+        self.ts.updateDelta(0.3)
+        self.ts.updateDelta(0.2)
+        check(self, self.lts, 0.5, 0.5)
+        self.ts.updateDelta(0.1)
+        check(self, self.lts, 0.6, 0.1)
 
 
 if __name__ == '__main__':
--- Assignments/4-Refactoring/time_sources.py
+++ Assignments/4-Refactoring/time_sources.py
@@ -25,6 +25,42 @@ class TimeSource:
 global_time = TimeSource()
 
 
+class StopWatch:
+    '''
+    A StopWatch re-references a parent_time_source (usually global_time) so
+    that it starts counting at t = 0 even if parent_time_source.getTime() != 0
+    when StopWatch.getTime() is first called. A StopWatch gets its time updates
+    from the parent_time_source.
+    '''
+    def __init__(self, parent_time_source=global_time):
+        self.ts = parent_time_source
+        self.parent_time_1 = 0.0
+        
+        self.first_run = True
+        self.time = 0.0
+        self.delta = 0.0
+    
+    def getTime(self):
+        self._update()
+        return self.time
+    def getDelta(self):
+        self._update()
+        return self.delta
+    
+    def _update(self):
+        parent_time = self.ts.getTime()
+        if self.first_run:
+            self.first_run = False
+            self.parent_time_1 = parent_time
+            self.delta = self.ts.getDelta()  # Use parent delta to start
+        
+        # If our data is stale, update it
+        if self.parent_time_1 != parent_time:
+            self.delta = parent_time - self.parent_time_1
+            self.time += self.delta
+            self.parent_time_1 = parent_time
+    
+
 # Don't use this function unless you're running a test. Production code expects
 # TimeSources to be monotonically increasing.
 def resetTimeSourceForTestingPurposes(ts):
