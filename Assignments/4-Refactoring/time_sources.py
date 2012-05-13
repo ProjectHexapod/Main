eede939c03a60a8d35a5ae18df686bf1762587a2
dydtc <dtydc5@gmail.com>
2012-05-13 19:48:46 +0000

    Added TimeSource.reset() and .updateDelta(). Renamed .update() to .updateTime()
    git-svn-id: http://projecthexapod.com/svn@168 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/time_sources.py
+++ Assignments/4-Refactoring/time_sources.py
@@ -1,11 +1,18 @@
 class TimeSource:
     def __init__(self, initial_time=0.0, initial_delta=0.0):
-        self.time = initial_time
-        self.delta = initial_delta
+        self.initial_time = initial_time
+        self.initial_delta = initial_delta
+        self.reset()
     
-    def update(self, time):
+    def reset(self):
+        self.time = self.initial_time
+        self.delta = self.initial_delta
+    def updateTime(self, time):
         self.delta = time - self.time
         self.time = time
+    def updateDelta(self, delta):
+        self.delta = delta
+        self.time += delta
     
     def getTime(self):
         return self.time
