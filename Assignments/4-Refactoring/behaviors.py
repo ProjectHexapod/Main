e850c3b968b68ecfd2390f1018bb184f88bddf1d
mattdunlap <mattdunlap@gmail.com>
2012-05-15 00:01:15 +0000

    refactored TrapezoidalFootMove to take acceleration into account.  Theres currently a bug in it somewhere, committing before bed, Ill deal with it tomorrow
    git-svn-id: http://projecthexapod.com/svn@189 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/behaviors.py
+++ Assignments/4-Refactoring/behaviors.py
@@ -1,4 +1,5 @@
 from math_utils import *
+from scipy.linalg import norm
 import time_sources
 
 
@@ -9,7 +10,7 @@ class PutFootOnGround:
         self.accel_duration = accel_duration
         
         self.done = self.leg.isFootOnGround()
-        self.target_foot_pos = self.leg.getFootPos()
+        self.current_foot_pos = self.leg.getFootPos()
         self.stop_watch = time_sources.StopWatch(active=False)
         if not self.done:
             self.stop_watch.smoothStart(self.accel_duration)
@@ -22,27 +23,28 @@ class PutFootOnGround:
             self.done = True
             self.stop_watch.smoothStop(self.accel_duration)
         if not self.isDone():
-            self.target_foot_pos[Z] -= self.vel * time_sources.global_time.getDelta()
-        return self.leg.jointAnglesFromFootPos(self.target_foot_pos)
+            self.current_foot_pos[Z] -= self.vel * time_sources.global_time.getDelta()
+        return self.leg.jointAnglesFromFootPos(self.current_foot_pos)
 
 
 class TrapezoidalFootMove:
     """This is a trapezoidal speed ramp, where speed is derivative foot position WRT time.
     """
-    def __init__(self, leg_controller, final_foot_pos, velocity, acceleration):
+    def __init__(self, leg_controller, final_foot_pos, max_velocity, acceleration):
         self.leg = leg_controller
         self.current_foot_pos = self.leg.getFootPos()
         self.final_foot_pos = final_foot_pos
-        self.vel = velocity
+        self.max_vel = max_velocity
+        self.vel = 0
         self.acc = acceleration
         
         # Unit vector pointing towards the destination
         self.dir = self.getNormalizedRemaining()
-        
+        self.stop_watch = time_sources.StopWatch(active=False)
         self.done = False
 
     def isDone(self):
-        return self.done
+        return self.done and not self.stop_watch.isActive()
 
     def getNormalizedRemaining(self):
         """Returns a normalized vector that points toward the current goal point.
@@ -50,11 +52,18 @@ class TrapezoidalFootMove:
         return normalize(self.final_foot_pos - self.current_foot_pos)
 
     def update(self):
-        if not self.done:
-            # Have we passed the destination?
-            if not arraysAreEqual(self.getNormalizedRemaining(), self.dir):
-                self.done = True
-                self.target_foot_pos = self.final_foot_pos
+        remaining_vector = self.final_foot_pos - self.current_foot_pos
+        if not self.done and not arraysAreEqual(self.getNormalizedRemaining(), self.dir):
+	    self.done = True
+            self.current_foot_pos = self.final_foot_pos
+        if not self.isDone():
+            delta = time_sources.global_time.getDelta()
+            # if the remaining distance <= the time it would take to slow down times the average speed during such a deceleration (ie the distance it would take to stop)
+            # rearranged multiplies to avoid confusing order of operations readability issues
+            if norm(remaining_vector) <= .5 * self.vel * self.vel / self.acc:
+                self.vel -= self.acc * delta
             else:
-                self.target_foot_pos += self.dir * self.vel * time_sources.global_time.getDelta()
-        return self.leg.jointAnglesFromFootPos(self.target_foot_pos)
+                self.vel += self.acc * delta
+                self.vel = min(self.vel, self.max_vel)
+            self.current_foot_pos += self.dir * self.vel * delta
+        return self.leg.jointAnglesFromFootPos(self.current_foot_pos)
