82577dcc04d2d29db7e4f52c3ac87e4d0ff5673e
dydtc <dtydc5@gmail.com>
2012-05-15 00:04:47 +0000

    Added PidController tests that verify the relationship between input/output functions.
    Fixed a bug in PidControllerTestCase.testUpdateDTerm().
    
    git-svn-id: http://projecthexapod.com/svn@191 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/test_pid_controller.py
+++ Assignments/4-Refactoring/test_pid_controller.py
@@ -37,7 +37,7 @@ class PidControllerTestCase(unittest.TestCase):
         self.pid.ki = 0
         global_time.updateDelta(0.1)
         large = self.pid.update(.1, 0)
-        self.pid.error_1 = .2
+        self.pid.prev_error = .2
         global_time.updateDelta(0.1)
         small = self.pid.update(.1, 0)
         self.assertTrue(small < large)
@@ -101,6 +101,84 @@ class PidControllerTestCase(unittest.TestCase):
         global_time.updateDelta(0.1)
         second = self.pid.update(-1, 0)
         self.assertEquals(first, second)
+    
+    def testTimeSeriesRampInput(self):
+        kp = 0.2
+        ki = 0.02
+        kd = 0.1
+        p = PidController(kp, 0.0, 0.0, -5000, 5000)
+        i = PidController(0.0, ki, 0.0, -5000, 5000)
+        d = PidController(0.0, 0.0, kd, -5000, 5000)
+        
+        t = 0.0
+        dt = 0.008
+        i_command_1 = 0.0
+        i_command_2 = 0.0
+        i_command_3 = 0.0
+        while t < 10.0:
+            t += dt
+            global_time.updateTime(t)
+            
+            desired = t
+            measured = -2.0*t
+            p_command = p.update(desired, measured)
+            i_command = i.update(desired, measured)
+            d_command = d.update(desired, measured)
+            pid_command = self.pid.update(desired, measured)
+            
+            self.assertAlmostEqual(p_command, kp * 3.0*t)
+            
+            # Testing the integral here is hard because of the discrete-time
+            # approximation.
+            
+            # 1st derivative is positive
+            self.assertGreater(i_command, i_command_1)
+            # 2nd derivative is positive
+            i_diff = i_command - i_command_1
+            i_diff_1 = i_command_1 - i_command_2
+            self.assertGreater(i_diff, i_diff_1)
+            # 3rd derivative is zero
+            i_diff_2 = i_command_2 - i_command_3
+            self.assertAlmostEqual(i_diff - i_diff_1, i_diff_1 - i_diff_2, 5)
+            
+            i_command_3 = i_command_2
+            i_command_2 = i_command_1
+            i_command_1 = i_command
+            
+            self.assertAlmostEqual(d_command, kd * 3.0)
+            self.assertAlmostEqual(pid_command, p_command + i_command + d_command)
+    def testTimeSeriesConstantInput(self):
+        kp = 0.2
+        ki = 0.02
+        kd = 0.1
+        p = PidController(kp, 0.0, 0.0, -5000, 5000)
+        i = PidController(0.0, ki, 0.0, -5000, 5000)
+        d = PidController(0.0, 0.0, kd, -5000, 5000)
+        
+        t = 0.0
+        dt = 0.12
+        while t < 10.0:
+            t += dt
+            global_time.updateTime(t)
+            
+            desired = -0.5
+            measured = 2.0
+            p_command = p.update(desired, measured)
+            i_command = i.update(desired, measured)
+            d_command = d.update(desired, measured)
+            pid_command = self.pid.update(desired, measured)
+            
+            self.assertAlmostEqual(p_command, kp * -2.5)
+            
+            # Because the input signal is constant, here the discrete-time
+            # integral is exact
+            self.assertAlmostEqual(i_command, ki * -2.5 * t)
+            
+            if (t == dt):  # First run
+                self.assertAlmostEqual(d_command, kd * -2.5 / dt)
+            else:
+                self.assertAlmostEqual(d_command, 0.0)
+            self.assertAlmostEqual(pid_command, p_command + i_command + d_command)
 
 
 if __name__ == '__main__':
