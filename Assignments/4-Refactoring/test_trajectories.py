a5c4e7bad3a59c5e93e22bb2a49db300f311c8a0
mattdunlap <mattdunlap@gmail.com>
2012-05-20 15:03:58 +0000

    added unit tests for trapezoidal foot movement
    git-svn-id: http://projecthexapod.com/svn@198 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/test_trajectories.py
+++ Assignments/4-Refactoring/test_trajectories.py
@@ -58,7 +58,55 @@ class TrapezoidalFootMoveTestCase(unittest.TestCase):
         trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, 1)
         self.assertFalse(trap.isDone())
         trap.target_foot_pos = array([.2, .2, -.1])
+        global_time.updateDelta(.1)
         trap.update()
         self.assertTrue(trap.isDone())
+
+    def testLeadingEdge(self):
+        # .19 here is .2 minus the time delta times the speed
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        mox.Replay(self.mockLegController)
+        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, 1)
+        global_time.updateDelta(.1)
+        trap.update()
+        # the lack of visible asserts here is because the verification of the mock object happens in tearDown
+
+    def testLeadingEdgeStrait(self):
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .14]))
+        mox.Replay(self.mockLegController)
+        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, 1)
+        trap.update()
+        trap.update()
+        trap.update()
+
+    def testPlateau(self):
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .18]))
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
+        mox.Replay(self.mockLegController)
+        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), .1, 1)
+        trap.update()
+        trap.update()
+        trap.update()
+
+    def testTriangleCase(self):
+        # Speed goes up from 1 to 2
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
+        # and then back down to 1
+        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .16]))
+        mox.Replay(self.mockLegController)
+        # never hitting the max speed of 10
+        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, .15]), 10, 1)
+        trap.update()
+        trap.update()
+        trap.update()
+        
+
+
+
+
 
 
