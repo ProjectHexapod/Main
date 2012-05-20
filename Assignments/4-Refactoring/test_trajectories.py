6b65734c8f982f7eea836b8020949b38dc46eeb9
dydtc <dtydc5@gmail.com>
2012-05-20 20:11:11 +0000

    Renamed mockLegController to mock_leg_controller because it's a variable.
    git-svn-id: http://projecthexapod.com/svn@203 c308ba4a-1832-4cc7-b836-58aacb0a83c6

--- Assignments/4-Refactoring/test_trajectories.py
+++ Assignments/4-Refactoring/test_trajectories.py
@@ -6,56 +6,60 @@ from time_sources import global_time
 from trajectories import PutFootOnGround, TrapezoidalFootMove
 import unittest
 
+
 class PutFootOnGroundTestCase(unittest.TestCase):
     def setUp(self):
-        self.mockLegController = mox.MockAnything()
-        self.mockLegController.isFootOnGround().AndReturn(False)
-        self.mockLegController.getFootPos().AndReturn([.1, 0., 0.])
+        self.mock_leg_controller = mox.MockAnything()
+        self.mock_leg_controller.isFootOnGround().AndReturn(False)
+        self.mock_leg_controller.getFootPos().AndReturn([.1, 0., 0.])
 
     def testIsDone(self):
-        mox.Replay(self.mockLegController)
-        pfog = PutFootOnGround(self.mockLegController, 1)
+        mox.Replay(self.mock_leg_controller)
+        pfog = PutFootOnGround(self.mock_leg_controller, 1)
         self.assertFalse(pfog.isDone())
         pfog.done = True
-        mox.Verify(self.mockLegController)
+        mox.Verify(self.mock_leg_controller)
 
     def testUpdateOnGround(self):
-        self.mockLegController.isFootOnGround().AndReturn(True)
-        self.mockLegController.jointAnglesFromFootPos([.1, 0, 0])
-        mox.Replay(self.mockLegController)
-        pfog = PutFootOnGround(self.mockLegController, 1)
+        self.mock_leg_controller.isFootOnGround().AndReturn(True)
+        self.mock_leg_controller.jointAnglesFromFootPos([.1, 0, 0])
+        mox.Replay(self.mock_leg_controller)
+        pfog = PutFootOnGround(self.mock_leg_controller, 1)
         self.assertFalse(pfog.isDone())
         pfog.update()
-        mox.Verify(self.mockLegController)
+        mox.Verify(self.mock_leg_controller)
 
     def testUpdateNotOnGround(self):
-        self.mockLegController.isFootOnGround().AndReturn(False)
-        self.mockLegController.jointAnglesFromFootPos([.1, 0, 0])
-        mox.Replay(self.mockLegController)
-        pfog = PutFootOnGround(self.mockLegController, 1)
+        self.mock_leg_controller.isFootOnGround().AndReturn(False)
+        self.mock_leg_controller.jointAnglesFromFootPos([.1, 0, 0])
+        mox.Replay(self.mock_leg_controller)
+        pfog = PutFootOnGround(self.mock_leg_controller, 1)
         global_time.updateDelta(.1)
         pfog.update()
-        mox.Verify(self.mockLegController)
+        mox.Verify(self.mock_leg_controller)
+
 
 class TrapezoidalFootMoveTestCase(unittest.TestCase):
     def setUp(self):
-        self.mockLegController = mox.MockAnything()
-        self.mockLegController.getFootPos().AndReturn(array([.2, .2, .2]))
+        self.mock_leg_controller = mox.MockAnything()
+        self.mock_leg_controller.getFootPos().AndReturn(array([.2, .2, .2]))
 
     def tearDown(self):
-        mox.Verify(self.mockLegController)
+        mox.Verify(self.mock_leg_controller)
 
     def testIsDone(self):
-        mox.Replay(self.mockLegController)
-        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, .1)
+        mox.Replay(self.mock_leg_controller)
+        trap = TrapezoidalFootMove(self.mock_leg_controller,
+                                   array([.2, .2, 0]), 10, .1)
         self.assertFalse(trap.isDone())
         trap.done = True
         self.assertTrue(trap.isDone())
 
     def testUpdateBecommingDone(self):
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, 0]))
-        mox.Replay(self.mockLegController)
-        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, 1)
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, 0]))
+        mox.Replay(self.mock_leg_controller)
+        trap = TrapezoidalFootMove(self.mock_leg_controller,
+                                   array([.2, .2, 0]), 10, 1)
         self.assertFalse(trap.isDone())
         trap.target_foot_pos = array([.2, .2, -.1])
         global_time.updateDelta(.1)
@@ -64,42 +68,47 @@ class TrapezoidalFootMoveTestCase(unittest.TestCase):
 
     def testLeadingEdge(self):
         # .19 here is .2 minus the time delta times the speed
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
-        mox.Replay(self.mockLegController)
-        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, 1)
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        mox.Replay(self.mock_leg_controller)
+        trap = TrapezoidalFootMove(self.mock_leg_controller,
+                                   array([.2, .2, 0]), 10, 1)
         global_time.updateDelta(.1)
         trap.update()
-        # the lack of visible asserts here is because the verification of the mock object happens in tearDown
+        # the lack of visible asserts here is because the verification of the
+        # mock object happens in tearDown
 
     def testLeadingEdgeStrait(self):
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .14]))
-        mox.Replay(self.mockLegController)
-        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), 10, 1)
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .14]))
+        mox.Replay(self.mock_leg_controller)
+        trap = TrapezoidalFootMove(self.mock_leg_controller,
+                                   array([.2, .2, 0]), 10, 1)
         trap.update()
         trap.update()
         trap.update()
 
     def testPlateau(self):
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .18]))
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
-        mox.Replay(self.mockLegController)
-        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, 0]), .1, 1)
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .18]))
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
+        mox.Replay(self.mock_leg_controller)
+        trap = TrapezoidalFootMove(self.mock_leg_controller,
+                                   array([.2, .2, 0]), .1, 1)
         trap.update()
         trap.update()
         trap.update()
 
     def testTriangleCase(self):
         # Speed goes up from 1 to 2
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .19]))
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .17]))
         # and then back down to 1
-        self.mockLegController.jointAnglesFromFootPos(ArraysEqual([.2, .2, .16]))
-        mox.Replay(self.mockLegController)
+        self.mock_leg_controller.jointAnglesFromFootPos(ArraysEqual([.2, .2, .16]))
+        mox.Replay(self.mock_leg_controller)
         # never hitting the max speed of 10
-        trap = TrapezoidalFootMove(self.mockLegController, array([.2, .2, .15]), 10, 1)
+        trap = TrapezoidalFootMove(self.mock_leg_controller,
+                                   array([.2, .2, .15]), 10, 1)
         trap.update()
         trap.update()
         trap.update()
