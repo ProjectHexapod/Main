4eedb17201aa402ae295b1062e9de6ea591050a8
Dan Cody <dtydc5@gmail.com>
2012-05-28 12:25:33 -0400

    added Pause trajectory that holds constant joint angles for a specified duration

--- Assignments/4-Refactoring/trajectories.py
+++ Assignments/4-Refactoring/trajectories.py
@@ -80,3 +80,18 @@ class TrapezoidalFootMove:
                 self.target_foot_pos = self.final_foot_pos
 
         return self.leg.jointAnglesFromFootPos(self.target_foot_pos)
+
+class Pause:
+    def __init__(self, leg_controller, duration):
+        self.leg = leg_controller
+        self.initial_angles = self.leg.getJointAngles()
+        self.duration = duration
+        self.sw = time_sources.StopWatch();
+        self.done = False
+        
+    def isDone(self):
+        return self.done
+    
+    def update(self):
+        self.done = self.sw.getTime() >= self.duration
+        return self.initial_angles
