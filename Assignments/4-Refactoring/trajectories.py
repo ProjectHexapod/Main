cbe5eee1d372fcc079d0767302211b9d28b8c458
Dan Cody <dtydc5@gmail.com>
2012-05-28 14:32:34 -0400

    Added FindJointStop trajectory.

--- Assignments/4-Refactoring/trajectories.py
+++ Assignments/4-Refactoring/trajectories.py
@@ -122,4 +122,30 @@ class MoveJoint:
 #                self.sw.smoothStop(self.accel_duration)
         
         return self.target_angles
+
+class FindJointStop:
+    def __init__(self, leg_controller, joint_idx, direction, velocity=0.005, accel_duration=0.25):
+        self.leg = leg_controller
+        self.joint = joint_idx
+        self.vel = direction * velocity
+        
+        self.target_angles = self.leg.getJointAngles()
+        self.sw = time_sources.StopWatch()
+        self.sw.smoothStart(accel_duration)
+        
+        self.moving = False
+        self.done = False
+    
+    def isDone(self):
+        return self.done
+        
+    def update(self):
+        if self.moving and not self.leg.isMoving():
+            self.done = True
+        if not self.done:
+            self.moving = self.leg.isMoving()
+            self.target_angles[self.joint] += self.sw.getDelta() * self.vel
+            
+        return self.target_angles
+
     
