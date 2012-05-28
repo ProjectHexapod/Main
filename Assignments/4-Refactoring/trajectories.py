836da2e3b33e842fd28a9c211643bc9b1bafe1b6
Dan Cody <dtydc5@gmail.com>
2012-05-28 13:06:11 -0400

    Added MoveJoint trajectory to move a single joint for a given duration.

--- Assignments/4-Refactoring/trajectories.py
+++ Assignments/4-Refactoring/trajectories.py
@@ -95,3 +95,31 @@ class Pause:
     def update(self):
         self.done = self.sw.getTime() >= self.duration
         return self.initial_angles
+
+class MoveJoint:
+    def __init__(self, leg_controller, joint_idx, duration, direction, velocity=0.01, accel_duration=0.25):
+        assert abs(direction) == 1
+        
+        self.leg = leg_controller
+        self.joint = joint_idx
+        self.duration = duration
+        self.vel = direction * velocity
+        self.accel_duration = accel_duration
+        
+        self.target_angles = self.leg.getJointAngles()
+        
+        self.sw = time_sources.StopWatch()
+        self.sw.smoothStart(self.accel_duration)
+        
+    def isDone(self):
+        return not self.sw.isActive()
+    
+    def update(self):
+        if not self.isDone():
+            self.target_angles[self.joint] += self.sw.getDelta() * self.vel
+            if self.sw.getTime() >= self.duration:
+                self.sw.stop()
+#                self.sw.smoothStop(self.accel_duration)
+        
+        return self.target_angles
+    
