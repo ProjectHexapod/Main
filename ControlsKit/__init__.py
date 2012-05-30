11d9f57c45f2193a5218ea763122e2204240b43c
Dan Cody <dtydc5@gmail.com>
2012-05-30 16:31:29 -0400

    Added module init for ControlsKit

---
+++ ControlsKit/__init__.py
@@ -0,0 +1,7 @@
+import filters
+from leg_controller import LegController
+from leg_logger import logger
+import math_utils
+from pid_controller import PidController
+import time_sources
+import trajectories
