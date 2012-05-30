87df63b0a6125c3886257c9213a1799508e401a6
Dan Cody <cody@nickel.(none)>
2012-05-29 21:49:04 -0400

    Added asymetric deadband compensation to ValveController.

--- Assignments/4-Refactoring/gimpy.py
+++ Assignments/4-Refactoring/gimpy.py
@@ -9,15 +9,22 @@ from move_one_joint import update
 #from hold_position import update
 #from knee_pid import update
 #from zero_flow_rate import update
+#from calibrate_deadband import update
+#from bleed import update
+
 
 leg1 = ControlBus(device='/dev/ttyUSB0')
-yaw_valve = ValveNode(leg1, node_id=1, name="yaw_valve", bore=0.0254, rod=0.0159, lpm=22.712, deadband=130)
-pitch_valve = ValveNode(leg1, node_id=2, name="pitch_valve", bore=0.0381, rod=0.0254, lpm=22.712, deadband=130)
-knee_valve = ValveNode(leg1, node_id=3, name="knee_valve", bore=0.0254, rod=0.0159, lpm=22.712, deadband=130)
+yaw_valve = ValveNode(leg1, node_id=1, name="yaw_valve", bore=0.0254, rod=0.0159, lpm=22.712, e_deadband=130, r_deadband=130)
+pitch_valve = ValveNode(leg1, node_id=2, name="pitch_valve", bore=0.0381, rod=0.0254, lpm=22.712, e_deadband=170, r_deadband=170)
+knee_valve = ValveNode(leg1, node_id=3, name="knee_valve", bore=0.0254, rod=0.0159, lpm=22.712, e_deadband=105, r_deadband=140)
 yaw_encoder = EncoderNode(leg1, node_id=4, name="yaw_encoder", gain=1, offset=-90*pi/180, bias=(1200,1200))
 pitch_encoder = EncoderNode(leg1, node_id=5, name="pitch_encoder", gain=-1, offset=40*pi/180, bias=(1200,1200))
 knee_encoder = EncoderNode(leg1, node_id=6, name="knee_encoder", gain=1, offset=220*pi/180, bias=(1200,1200))
 shock_encoder = EncoderNode(leg1, node_id=7, name="shock_encoder", gain=1, offset=0, bias=(1200,1200))
+
+print "YAW: e=%f, r=%f" % (yaw_valve.max_extend_rate, yaw_valve.max_retract_rate)
+print "HP: e=%f, r=%f" % (pitch_valve.max_extend_rate, pitch_valve.max_retract_rate)
+print "KP: e=%f, r=%f" % (knee_valve.max_extend_rate, knee_valve.max_retract_rate)
 
 time_1 = 0.0
 try:
