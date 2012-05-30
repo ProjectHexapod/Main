ab408458a4f42ad7709d5390e104f4f43478b621
Dan Cody <dtydc5@gmail.com>
2012-05-30 19:25:34 -0400

    Renamed sensor readings to indicate they are from an IMU.

--- Stompy/stand_sit.py
+++ Stompy/stand_sit.py
@@ -8,11 +8,11 @@ traj = None
 state = 0
 
 
-def update(time, leg_sensor_matrix, orientation, angular_rates):
+def update(time, leg_sensor_matrix, imu_orientation, imu_angular_rates):
     global traj, state
     
     time_sources.global_time.updateTime(time)
-    body.setBodyState(leg_sensor_matrix, orientation, angular_rates)
+    body.setBodyState(leg_sensor_matrix, imu_orientation, imu_angular_rates)
     
     joint_angle_matrix = zeros((NUM_LEGS, LEG_DOF))
     body.setDesiredJointAngles(joint_angle_matrix)
