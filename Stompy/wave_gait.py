24a8d0b73bf57ac3d6830bcc85e9157f331cbb7e
M@ <matt@xtionTest.(none)>
2012-06-17 10:29:10 -0400

    wave gate progress

--- Stompy/wave_gait.py
+++ Stompy/wave_gait.py
@@ -1,15 +1,49 @@
 from ControlsKit import time_sources, BodyController
+from ControlsKit.leg_trajectories import Pause, TrapezoidalFootMove
 from ControlsKit.math_utils import NUM_LEGS, LEG_DOF
 from scipy import zeros
 
 body = BodyController()
 traj = None
 state = 0
+leg_pair = 0
 stride = 1  # in meters
 
 def update(time, leg_sensor_matrix, imu_orientation, imu_accelerations, imu_angular_rates):
-    global traj, state
+    global traj, legs
 
     time_sources.global_time.updateTime(time)
     body.setSensorReadings(leg_sensor_matrix, imu_orientation, imu_accelerations, imu_angular_rates)
-    # TODO: write me
+    legs = body.getLegs()
+
+    if traj is None:
+        traj = Pause(leg, 1.0)
+
+    if traj.isDone():
+        pass
+    # TODO: once the IK takes joint limits into account check it to see if the joint can go stride further
+    # TODO: move each pair of legs forward by stride
+    # TODO: with the feet on the ground move all of the feet backwards by stride
+
+def getLeftAndRightLegsInPair(legs, pair):
+    pair %= NUM_LEGS
+    return legs[pair], legs[NUM_LEGS - pair - 1]
+
+def enterState(state, legs, pair):
+    left, right = getLeftAndRightLegsInPair(legs, pair)
+    if state == 0:  # move all legs back by stride
+        pass
+    elif state == 1:
+        pass
+    elif state == 2:
+        pass
+    elif state == 3:
+        pass
+    elif state == 4:
+        pass
+
+
+
+
+
+
