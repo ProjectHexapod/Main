0084c7e0159bac1e7ddfce6c5c13655c3a7973a5
Jona Raphael <jona.raphael@gmail.com>
2012-06-17 10:37:48 -0400

    refactored turn in place

--- Stompy/turn_in_place.py
+++ Stompy/turn_in_place.py
@@ -1,17 +1,17 @@
-from ControlsKit import time_sources, BodyController
+from ControlsKit import time_sources, BodyModel
 from ControlsKit.math_utils import NUM_LEGS, LEG_DOF
 from scipy import zeros
-from ControlsKit.leg_trajectories import TrapezoidalFootMove
+from ControlsKit.leg_paths import TrapezoidalFootMove
 
-body = BodyController()
-traj = None
+body = BodyModel()
+path = None
 state = 0
 
 TURN = 1
 RESOLVE = 2
 
 def update(time, leg_sensor_matrix, imu_orientation, imu_accelerations, imu_angular_rates):
-    global traj, state
+    global path, state
     
     time_sources.global_time.updateTime(time)
     body.setSensorReadings(leg_sensor_matrix, imu_orientation, imu_angular_rates)
