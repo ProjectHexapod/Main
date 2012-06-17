e9f86c433c46b22df8932ebb561ae81ccd4185ff
Jona Raphael <jona.raphael@gmail.com>
2012-06-17 18:00:29 -0400

    FINSISHED REFACTORING LEG STUFF.      AYYYYYYYY

--- Gimpy/planners/row_the_cart.py
+++ Gimpy/planners/row_the_cart.py
@@ -1,4 +1,4 @@
-from ControlsKit import time_sources, LegModel, logger, LimbController
+from ControlsKit import time_sources, LegModel, leg_logger, LimbController
 from ControlsKit.Paths import Pause, TrapezoidalFootMove, PutFootOnGround
 from ControlsKit.math_utils import array, Z
 
@@ -6,7 +6,7 @@ from ControlsKit.math_utils import array, Z
 # Initialization
 model = LegModel()
 controller = LimbController()
-traj = None
+path = None
 
 S_MOVE1 = 0
 S_LOWER = 1
@@ -18,7 +18,7 @@ state = S_MOVE3
 
 # Body of control loop
 def update(time, yaw, hip_pitch, knee_pitch, shock_depth):
-    global traj, state
+    global path, state
     
     
     # Update model
@@ -26,32 +26,32 @@ def update(time, yaw, hip_pitch, knee_pitch, shock_depth):
     model.setSensorReadings(yaw, hip_pitch, knee_pitch, shock_depth)
     model.updateFootOnGround()
 
-    # Init traj. Do this after the first update.
-    if traj is None:
-        traj = Pause(model, controller, 1.0)
+    # Init path. Do this after the first update.
+    if path is None:
+        path = Pause(model, controller, 1.0)
     
-    # Monitor leg_trajectories
-    if traj.isDone():
+    # Monitor leg_paths
+    if path.isDone():
         if state == S_MOVE3:
-            traj = TrapezoidalFootMove(model, controller,
+            path = TrapezoidalFootMove(model, controller,
                                        array([1.5, -0.6, -0.4]),
                                        0.2, 0.1)
             state = S_MOVE1
         elif state == S_MOVE1:
-            traj = PutFootOnGround(model, controller, 0.05)
+            path = PutFootOnGround(model, controller, 0.05)
             state = S_LOWER
         elif state == S_LOWER:
             ground_level = model.getFootPos()[Z]
-            traj = TrapezoidalFootMove(model, controller,
+            path = TrapezoidalFootMove(model, controller,
                                        array([1.5, .6, ground_level]),
                                        0.2, 0.1)
             state = S_MOVE2
         elif state == S_MOVE2:
-            traj = TrapezoidalFootMove(model, controller,
+            path = TrapezoidalFootMove(model, controller,
                                        array([1.5, 0.6, -0.4]),
                                        0.2, 0.1)
             state = S_MOVE3
-        logger.info("State changed.", state=state)
+        leg_logger.logger.info("State changed.", state=state)
 
     # Evaluate path and joint control
     controller.update(model.getJointAngles(), path.update())
