e49665bd9ada88489509527d4da0dd879f80c83d
Jona Raphael <jona.raphael@gmail.com>
2012-06-17 16:11:38 -0400

    refactring

--- ControlsKit/Paths/put_foot_on_ground.py
+++ ControlsKit/Paths/put_foot_on_ground.py
@@ -1,29 +1,54 @@
 from ControlsKit import time_sources, leg_logger
 from ControlsKit.math_utils import Z
-
+from ConfigParser import ConfigParser
+from os import path
 
 class PutFootOnGround:
-    def __init__(self, leg_model, velocity, accel_duration=0.1):
+    def __init__(self, leg_model, limb_controller, velocity, accel_duration=0.1):
         logger.info("New trajectory.", traj_name="PutFootOnGround",
                     velocity=velocity, accel_duration=accel_duration)
         
-        self.leg = leg_model
+        self.model = leg_model
+        self.controller = limb_controller
         self.vel = velocity
         self.accel_duration = accel_duration
         
-        self.done = self.leg.isFootOnGround()
-        self.target_foot_pos = self.leg.getFootPos()
+        self.done = self.model.isFootOnGround()
+        self.target_foot_pos = self.model.getFootPos()
         self.stop_watch = time_sources.StopWatch(active=False)
         if not self.done:
             self.stop_watch.smoothStart(self.accel_duration)
+        
+        # Set PID gains for this path
+        config_file="../ControlsKit/leg_model.conf"
+        section="LegModel"
+        c = ConfigParser()
+        if not path.exists(path.abspath(config_file)):
+            print 'Config file %s not found!'%config_file
+            raise IOError
+        c.read(config_file)
+        self.controller.updateGainConstants([c.getfloat(section, "yaw_p"),  # proportional terms
+                        c.getfloat(section, "hp_p"),
+                        c.getfloat(section, "kp_p")],
+        
+                        [c.getfloat(section, "yaw_i"),  # integral terms
+                        c.getfloat(section, "hp_i"),
+                        c.getfloat(section, "kp_i")],
+        
+                        [c.getfloat(section, "yaw_d"),  # differential terms
+                        c.getfloat(section, "hp_d"),
+                        c.getfloat(section, "kp_d")] )
+        
+        self.sw = time_sources.StopWatch()
+        self.sw.smoothStart(self.accel_duration)
 
     def isDone(self):
         return self.done and not self.stop_watch.isActive()
 
     def update(self):
-        if not self.done and self.leg.isFootOnGround():
+        if not self.done and self.model.isFootOnGround():
             self.done = True
             self.stop_watch.smoothStop(self.accel_duration)
         if not self.isDone():
             self.target_foot_pos[Z] -= self.vel * self.stop_watch.getDelta()
-        return self.leg.jointAnglesFromFootPos(self.target_foot_pos)
+        return self.model.jointAnglesFromFootPos(self.target_foot_pos)
--- ControlsKit/Paths/trapezoidal_foot_move.py
+++ ControlsKit/Paths/trapezoidal_foot_move.py
@@ -1,17 +1,20 @@
 from ControlsKit import time_sources, leg_logger
 from ControlsKit.math_utils import normalize, norm, arraysAreEqual
+from ConfigParser import ConfigParser
+from os import path
 
 
 class TrapezoidalFootMove:
     """This is a trapezoidal speed ramp, where speed is derivative foot position WRT time. 
     """
-    def __init__(self, leg_model, final_foot_pos, max_velocity, acceleration):
+    def __init__(self, leg_model, limb_controller, final_foot_pos, max_velocity, acceleration):
         leg_logger.logger.info("New trajectory.", traj_name="TrapezoidalFootMove",
                     final_foot_pos=final_foot_pos, max_velocity=max_velocity,
                     acceleration=acceleration)
         
-        self.leg = leg_model
-        self.target_foot_pos = self.leg.getFootPos()
+        self.model = leg_model
+        self.controller = limb_controller
+        self.target_foot_pos = self.model.getFootPos()
         self.final_foot_pos = final_foot_pos
         self.max_vel = max_velocity
         self.vel = 0.0
@@ -20,6 +23,29 @@ class TrapezoidalFootMove:
         # Unit vector pointing towards the destination
         self.dir = self.getNormalizedRemaining()
         self.done = False
+                
+        # Set PID gains for this path
+        config_file="../ControlsKit/leg_model.conf"
+        section="LegModel"
+        c = ConfigParser()
+        if not path.exists(path.abspath(config_file)):
+            print 'Config file %s not found!'%config_file
+            raise IOError
+        c.read(config_file)
+        self.controller.updateGainConstants([c.getfloat(section, "yaw_p"),  # proportional terms
+                        c.getfloat(section, "hp_p"),
+                        c.getfloat(section, "kp_p")],
+        
+                        [c.getfloat(section, "yaw_i"),  # integral terms
+                        c.getfloat(section, "hp_i"),
+                        c.getfloat(section, "kp_i")],
+        
+                        [c.getfloat(section, "yaw_d"),  # differential terms
+                        c.getfloat(section, "hp_d"),
+                        c.getfloat(section, "kp_d")] )
+        
+        self.sw = time_sources.StopWatch()
+        self.sw.smoothStart(self.accel_duration)
 
     def isDone(self):
         return self.done
@@ -50,4 +76,4 @@ class TrapezoidalFootMove:
                 self.done = True
                 self.target_foot_pos = self.final_foot_pos
 
-        return self.leg.jointAnglesFromFootPos(self.target_foot_pos)
+        return self.model.jointAnglesFromFootPos(self.target_foot_pos)
