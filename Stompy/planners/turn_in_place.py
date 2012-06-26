9e05a2d930cc839d0b42c3faa73b2f67f9bfeef4
George Sass <george.a.sass@gmail.com>
2012-06-25 23:32:18 -0400

    =Updated turn_in_place.py to the point where it is successfully parsed by the simulator.

--- Stompy/planners/turn_in_place.py
+++ Stompy/planners/turn_in_place.py
@@ -6,7 +6,7 @@ from ControlsKit.body_paths import TrapezoidalFeetAlign, TrapezoidalFeetLiftLowe
 from ControlsKit.leg_paths import TrapezoidalFootMove
 
 controller=BodyController()
-body = BodyModel()
+model = BodyModel()
 path = None
 state = 0
 
@@ -20,32 +20,32 @@ def update(time, leg_sensor_matrix, imu_orientation, imu_accelerations, imu_angu
     global path, state
     
     time_sources.global_time.updateTime(time)
-    body.setSensorReadings(leg_sensor_matrix, imu_orientation, imu_angular_rates)
+    model.setSensorReadings(leg_sensor_matrix, imu_orientation, imu_angular_rates)
     
     if path is None:
         path = BodyPause(model, controller, .1)
-        state = TURN_FIRST_TRIPOD
+        state = RAISE_FIRST_TRIPOD
     
     if path.isDone():
         if state==RAISE_FIRST_TRIPOD:
             path = TrapezoidalFeetLiftLower(model, controller, [0,2,4], .3, 2, 1)
             state=TURN_FIRST_TRIPOD
         if state==TURN_FIRST_TRIPOD:
-            RotateFeetAboutOrigin(model, controller, [0,2,4], .2, 2, 1)
+            path = RotateFeetAboutOrigin(model, controller, [0,2,4], .2, 2, 1)
             state=RAISE_SECOND_TRIPOD
         if state==RAISE_SECOND_TRIPOD:
             path = TrapezoidalFeetLiftLower(model, controller, [1,3,5], .3, 2, 1)
             state=TURN_SECOND_TRIPOD
         if state==TURN_SECOND_TRIPOD:
-            RotateFeetAboutOrigin(model, controller, [1,3,5], .2, 2, 1)
+            path = RotateFeetAboutOrigin(model, controller, [1,3,5], .2, 2, 1)
             state=RESOLVE
         if state==RESOLVE:
-            RotateFeetAboutOrigin(model, controller, range(5), -.2, 2, 1)
+            path = RotateFeetAboutOrigin(model, controller, range(5), -.2, 2, 1)
             state=RAISE_FIRST_TRIPOD
     
     
     target_angle_matrix = path.update()
-    body.setDesiredJointAngles(joint_angle_matrix)
+    #controller.setDesiredJointAngles(joint_angle_matrix)
     
     # Send commands
     return controller.update(model.getJointAngleMatrix(), target_angle_matrix)
