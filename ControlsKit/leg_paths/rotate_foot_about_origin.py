29dbbd5721cb791897aca5c80068ec9131de81af
Jona Raphael <jona.raphael@gmail.com>
2012-06-27 23:38:45 -0400

    debug version of rotate_foot_about_origin

--- ControlsKit/leg_paths/rotate_foot_about_origin.py
+++ ControlsKit/leg_paths/rotate_foot_about_origin.py
@@ -1,5 +1,5 @@
 from ControlsKit import time_sources, leg_logger
-from ControlsKit.math_utils import normalize, norm, arraysAreEqual
+from ControlsKit.math_utils import normalize, norm, arraysAreEqual, rotateZ, array
 from numpy import arctan2, sign, pi, cos, sin
 
 class RotateFootAboutOrigin:
@@ -10,7 +10,10 @@ class RotateFootAboutOrigin:
         leg_logger.logger.info("New path.", path_name="RotateFootAboutOrigin",
                     delta_angle=delta_angle, max_velocity=max_velocity,
                     acceleration=acceleration)
-
+        
+        self.file_name = "rotate_foot_about_origin_data_leg%d.csv" % leg_index
+        self.file = open(self.file_name, "a")
+        
         self.body_model = body_model
         self.leg_index = leg_index
         self.leg_model = self.body_model.getLegs()[self.leg_index]
@@ -21,6 +24,7 @@ class RotateFootAboutOrigin:
         self.max_ang_vel = max_velocity
         self.ang_vel = 0.0
         self.ang_acc = acceleration
+        
         
         self.body_coord = body_model.transformLeg2Body(self.leg_index, self.leg_model.footPosFromLegState([self.limb_controller.getDesiredPosAngle(), self.leg_model.getShockDepth()]))
         self.init_angle = arctan2(self.body_coord[1], self.body_coord[0])
@@ -47,8 +51,8 @@ class RotateFootAboutOrigin:
             # down times the average speed during such a deceleration (ie
             # the distance it would take to stop)
             
+            remaining_angle = self.delta_angle + self.init_angle - self.last_commanded_angle
             self.target_angle = self.last_commanded_angle
-            remaining_angle = self.delta_angle + self.init_angle - self.target_angle
             
             if norm(remaining_angle) <= .5 * self.ang_vel**2 / self.ang_acc:
                 self.ang_vel -= self.ang_acc * delta
@@ -59,8 +63,23 @@ class RotateFootAboutOrigin:
             self.last_commanded_angle = self.target_angle
             self.target_foot_pos = [self.radius*cos(self.target_angle), self.radius*sin(self.target_angle), self.init_height]
             
+            if self.leg_index == 0:
+                leg_coord = self.leg_model.getFootPos()
+                hip_offset = self.body_model.getHipOffset(self.leg_index)
+                rotated = rotateZ(leg_coord,hip_offset[2])
+                print(leg_coord)
+                print("rotated", rotated)
+                body_coord = rotated+array([hip_offset[0],hip_offset[1],0])
+                #print("body", norm(body_coord))
+                #print("workedout position", body_coord)
+                #print("transformed position", self.body_model.transformLeg2Body(0,leg_coord))
+                
             if not sign(remaining_angle) == self.dir:
                 self.done = True
                 self.target_foot_pos = [self.radius*cos(self.delta_angle + self.init_angle), self.radius*sin(self.delta_angle + self.init_angle), self.init_height]
+        a = self.body_model.transformLeg2Body(self.leg_index,self.leg_model.getFootPos())
+        b = self.target_foot_pos
+        
+        self.file.write("%f,%f,%f,%f,%f,%f\n" % (a[0],a[1],a[2],b[0],b[1],b[2]))
 
         return self.leg_model.jointAnglesFromFootPos(self.body_model.transformBody2Leg(self.leg_index, self.target_foot_pos))
