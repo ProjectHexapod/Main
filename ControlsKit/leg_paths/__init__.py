9397e4257dce22e91e4688477fb4514e84942c9c
Dan Cody <dtydc5@gmail.com>
2012-07-01 18:21:20 -0400

    Alphabetized leg_paths/__init__.py.

--- ControlsKit/leg_paths/__init__.py
+++ ControlsKit/leg_paths/__init__.py
@@ -3,6 +3,6 @@ from interpolated_foot_move import InterpolatedFootMove
 from move_joint import MoveJoint
 from pause import Pause
 from put_foot_on_ground import PutFootOnGround
+from rotate_foot_about_origin import RotateFootAboutOrigin
 from trapezoidal_foot_move import TrapezoidalFootMove
 from trapezoidal_joint_move import TrapezoidalJointMove
-from rotate_foot_about_origin import RotateFootAboutOrigin
