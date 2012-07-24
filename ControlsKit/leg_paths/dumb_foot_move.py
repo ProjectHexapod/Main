f5b5aa090292c4312b7878a69b869ca5b5e39780
jwhong <jwhong@jwhong-ThinkPad-T520.(none)>
2012-07-24 17:20:14 -0400

    Added DumbFootMove, which is designed to simply track a point which is constantly being changed by the higher level controller

--- ControlsKit/leg_paths/dumb_foot_move.py
+++ ControlsKit/leg_paths/dumb_foot_move.py
@@ -10,12 +10,12 @@ class DumbFootMove:
     
     """
     def __init__(self, leg_model, limb_controller, target_pos):
-        logger.info("New trajectory.", traj_name="DumbFootMove",
-                    final_foot_pos=final_foot_pos)
-        
         self.model = leg_model
         self.controller = limb_controller
-        self.target_foot_pos = target_foot_pos
+        self.target_foot_pos = target_pos
+
+    def setTarget( self, new_target ):
+        self.target_foot_pos = new_target
 
     def isDone(self):
         return False
