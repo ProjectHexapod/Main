7d8412661d3fbf29e8e57c1dfbdd238c973552ef
James Whong <james.whong@gmail.com>
2013-02-25 21:15:44 -0500

    Removed Windows functionality, increased select timeout from .1 to 1.0

--- Protoleg/getch.py
+++ Protoleg/getch.py
@@ -2,13 +2,9 @@ class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
     def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
+        self.impl = _GetchUnix()
 
     def __call__(self): return self.impl()
-
 
 class _GetchUnix:
     def __init__(self):
@@ -26,15 +22,5 @@ class _GetchUnix:
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
-
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
 
 getch = _Getch()
