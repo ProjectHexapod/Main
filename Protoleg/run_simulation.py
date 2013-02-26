13782a3b69203df1b2964afab4b3beacb341bffc
James Whong <james.whong@gmail.com>
2013-02-25 21:16:29 -0500

    Set up simulation to use LegOnColumn

--- Protoleg/run_simulation.py
+++ Protoleg/run_simulation.py
@@ -2,9 +2,8 @@ import sys
 sys.path.append('..')
 sys.path.append('planners')
 from SimulationKit import Simulator
-from SimulationKit.Robots import LegOnStand
+from SimulationKit.Robots import LegOnColumn
 from SimulationKit.helpers import *
-from ControlsKit import leg_logger as logger
 from ControlsKit.import_planner import importPlanner
 from UI import InputServer
 
@@ -18,28 +17,25 @@ else:
     update = retval
     controller = None
 
-d = {'offset':(0,0,0.67)}
-s = Simulator(dt=1e-3,plane=1,pave=0,graphical=1,robot=LegOnStand,robot_kwargs=d, start_paused = True)
-input_server = InputServer()  # TODO: pass along at least a password argument here
-input_server.startListening()
+d = {'offset':(0,0,1.67)}
+s = Simulator(dt=1e-3,plane=1,pave=0,graphical=1,robot=LegOnColumn,robot_kwargs=d, start_paused = True, render_objs=1, draw_contacts=1)
 
-yaw_joint = s.robot.members['hip_yaw']
-pitch_joint = s.robot.members['hip_pitch']
-knee_joint = s.robot.members['knee_pitch']
-shock_joint = s.robot.members['foot_shock']
+yaw_joint   = s.robot.joints['hip_yaw']
+pitch_joint = s.robot.joints['hip_pitch']
+knee_joint  = s.robot.joints['knee_pitch']
+shock_joint = s.robot.joints['foot_shock']
+
 try:
     while True:
         s.step()
         if not s.getPaused():
             time = s.getSimTime()
-            command = input_server.getLastCommand()
-            lr = update(time, yaw_joint.getAngle(), pitch_joint.getAngle(), knee_joint.getAngle(), shock_joint.getPosition(), command)
+            lr = update(time, yaw_joint.getAngle(), pitch_joint.getAngle(), knee_joint.getAngle(), shock_joint.getPosition())
             yaw_joint.setLengthRate(lr[0])
             pitch_joint.setLengthRate(lr[1])
             knee_joint.setLengthRate(lr[2])
 
 except:
-    input_server.stopListening()
     yaw_joint.setLengthRate(0)
     pitch_joint.setLengthRate(0)
     knee_joint.setLengthRate(0)
