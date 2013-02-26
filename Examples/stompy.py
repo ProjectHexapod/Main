163504449b34731155711ad59bcdc06d23b4a756
James Whong <james.whong@gmail.com>
2013-02-26 01:07:02 -0500

    Changed default arguments to simulator

--- Examples/stompy.py
+++ Examples/stompy.py
@@ -4,8 +4,9 @@ from SimulationKit import Simulator
 from SimulationKit.Robots import SpiderWHydraulics
 
 d = {'offset':(0,0,1.5)}
-s = Simulator(dt=2e-3,plane=1,pave=0,graphical=1,ground_grade=.00,robot=SpiderWHydraulics,robot_kwargs=d,\
-    render_objs=0, draw_COM=1, draw_support=1, draw_contacts=0)
+print Simulator
+s = Simulator(dt=2e-3,plane=1,graphical=1,ground_grade=.00,robot=SpiderWHydraulics,robot_kwargs=d,\
+    render_objs=1, draw_COM=1, draw_support=1, draw_contacts=0, height_map=None)
 while True:
     s.step()
     s.robot.constantSpeedWalkSmart()
