84d049b5eea1fa7253389ee6f703b0a055b0238a
James Whong <james.whong@gmail.com>
2013-02-26 00:54:01 -0500

    Added rudimentary path

---
+++ Protoleg/planners/row_motion.py
@@ -0,0 +1,45 @@
+from ControlsKit import time_sources, LegModel, LimbController
+from ControlsKit.leg_paths import Pause, TrapezoidalFootMove
+#from ControlsKit.leg_paths import PutFootOnGround
+from ControlsKit.math_utils import array, Z
+#from UI import logger
+from ControlsKit.time_sources import global_time
+
+# Initialization
+model = LegModel()
+controller = LimbController()
+path = None
+
+points = []
+points.append( (1.6,  0.95, -1.15) )
+points.append( (1.6, -0.95, -1.15) )
+points.append( (1.6, -0.95, -1.58) )
+points.append( (1.6,  0.95, -1.58) )
+#points.append( (1.6,  0.00, -1.7) )
+index = 0
+
+# Body of control loop
+def update(time, yaw, hip_pitch, knee_pitch, shock_depth, command=None):
+    global points, path, index
+    
+    
+    # Update model
+    time_sources.global_time.updateTime(time)
+    model.setSensorReadings(yaw, hip_pitch, knee_pitch, shock_depth)
+    model.updateFootOnGround()
+
+    # Init path. Do this after the first update.
+    if path is None:
+        path = Pause(model, controller, 1.0)
+    
+    if path.isDone():
+        path = TrapezoidalFootMove(model, controller,
+                                       array(points[index]),
+                                       0.5, 0.4)
+        index = (index+1)%len(points)
+
+    # Evaluate path and joint control
+    controller.update(model.getJointAngles(), path.update())
+
+    # Send commands
+    return controller.getLengthRateCommands()
