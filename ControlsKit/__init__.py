0d5608a26fa87f69bdc2cd9aed01d308401817eb
James Whong <james.whong@gmail.com>
2013-02-26 01:03:40 -0500

    Commented out unused modules.

--- ControlsKit/__init__.py
+++ ControlsKit/__init__.py
@@ -1,9 +1,9 @@
-from body_model import BodyModel
-from body_controller import BodyController
-import body_paths
-import filters
+#from body_model import BodyModel
+#from body_controller import BodyController
+#import body_paths
+#import filters
 from leg_model import LegModel
-import leg_paths
-import math_utils
+#import leg_paths
+#import math_utils
 from limb_controller import LimbController
 import time_sources
