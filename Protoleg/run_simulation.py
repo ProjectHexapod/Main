6062e72686030e43561e76401f6767b007119e16
James Whong <james.whong@gmail.com>
2013-02-27 15:45:11 -0500

    Modified run_simulation to use valve commands instead of length rate commands

--- Protoleg/run_simulation.py
+++ Protoleg/run_simulation.py
@@ -24,25 +24,24 @@ else:
     controller = None
 
 d = {'offset':(0,0,2.00)}
-s = Simulator(dt=1e-3,plane=1,pave=0,graphical=1,robot=LegOnColumn,robot_kwargs=d, start_paused = True, render_objs=1, draw_contacts=1)
+s = Simulator(dt=1e-3,plane=1,pave=0,graphical=1,robot=LegOnColumn,robot_kwargs=d, start_paused = False, render_objs=1, draw_contacts=1)
 
 yaw_joint   = s.robot.joints['hip_yaw']
 pitch_joint = s.robot.joints['hip_pitch']
 knee_joint  = s.robot.joints['knee_pitch']
 shock_joint = s.robot.joints['foot_shock']
 
-try:
-    while True:
-        s.step()
-        if not s.getPaused():
-            time = s.getSimTime()
-            lr = update(time, yaw_joint.getAngle(), pitch_joint.getAngle(), knee_joint.getAngle(), shock_joint.getPosition())
-            yaw_joint.setLengthRate(lr[0])
-            pitch_joint.setLengthRate(lr[1])
-            knee_joint.setLengthRate(lr[2])
-
-except:
-    yaw_joint.setLengthRate(0)
-    pitch_joint.setLengthRate(0)
-    knee_joint.setLengthRate(0)
-    raise
+while True:
+    s.step()
+    if not s.getPaused():
+        time = s.getSimTime()
+        lr = update(time, yaw_joint.getAngle(), pitch_joint.getAngle(), knee_joint.getAngle(), shock_joint.getPosition())
+        #yaw_joint.setLengthRate(lr[0])
+        yaw_joint.setValveCommand(lr[0])
+        #pitch_joint.setLengthRate(lr[1])
+        pitch_joint.setValveCommand(lr[1])
+        #knee_joint.setLengthRate(lr[2])
+        knee_joint.setValveCommand(lr[2])
+        #yaw_joint.setValveCommand(0.5)
+        #pitch_joint.setValveCommand(1.0)
+        #knee_joint.setValveCommand(0.0)
