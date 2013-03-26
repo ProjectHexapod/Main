7dae89a2653e27cef2daae8edd5a995818f2ffec
James Whong <james.whong@gmail.com>
2013-03-26 16:52:46 -0400

    smart_move is now the recommended trajectory.  It uses real actuator geometry to figure out good feed-forward commands, resulting in much more stable and consistent control.

--- Protoleg/planners/smart_move.py
+++ Protoleg/planners/smart_move.py
@@ -2,7 +2,7 @@ from ControlsKit import time_sources, LegModel, LimbController
 from ControlsKit.leg_paths import Pause, TrapezoidalFootMove
 from ControlsKit.math_utils import array, Z
 from ControlsKit.time_sources import global_time
-from  joint_controller import PistonController
+from  joint_controller import PistonController, JointController
 from Utilities.ActuatorCharacteristics import *
 
 # Convenience multipliers...
@@ -21,15 +21,10 @@ class Gait(object):
         self.index = 0
         self.points = []
         #self.points.append( (0.4,  0, -0.45, 0.2) )
-        self.points.append( (1.8, -1.35, -0.75, 0.40) )
-        self.points.append( (1.8, -1.35, -1.68, 0.20) )
-        self.points.append( (1.8,  1.35, -1.68, 0.40) )
-        self.points.append( (1.8,  1.35, -0.75, 0.20) )
-
-        #self.points.append( (1.7, -1.25, -1.68, 0.30) )
-        #self.points.append( (1.7,  1.25, -1.68, 0.30) )
-        #self.points.append( (1.7, 0.0, -1.05, 0.15) )
-        #self.points.append( (1.7, 0.0, -1.68, 0.15) )
+        self.points.append( (1.8, -1.00, -0.75, 0.20) )
+        self.points.append( (1.8, -1.00, -1.68, 0.20) )
+        self.points.append( (1.8,  1.00, -1.68, 0.20) )
+        self.points.append( (1.8,  1.00, -0.75, 0.20) )
 
         # Describe the actuator placements at each joint
         YAW_ACT    = ActuatorMathHelper(
@@ -65,26 +60,36 @@ class Gait(object):
                 system_pressure        = psi2pascal*2300,
                 axis                   = ( 0, -1, 0 ) )
         self.act_helpers = [YAW_ACT, PITCH_ACT, KNEE_ACT]
-        
-        yaw_pist   = PistonController(YAW_ACT  , 0.26, 0.28)
+       
+        yaw_joint  = JointController(YAW_ACT,
+                kp=1.0,
+                ki=0.0,
+                kd=0.0,
+                kff=1.0,
+                kfa=0.0,
+                derivative_corner=10)
+        pitch_joint  = JointController(PITCH_ACT,
+                kp=3.5,
+                ki=0.0,
+                kd=0.0,
+                kff=1.0,
+                kfa=0.0,
+                derivative_corner=10)
+        knee_joint  = JointController(KNEE_ACT,
+                kp=3.5,
+                ki=0.0,
+                kd=0.0,
+                kff=1.0,
+                kfa=0.0,
+                derivative_corner=10)
+
+        self.joint_controllers = [yaw_joint, pitch_joint, knee_joint]
+
+        yaw_pist   = PistonController(YAW_ACT  , 0.26, 0.26)
         pitch_pist = PistonController(PITCH_ACT, 0.27, 0.27)
         knee_pist  = PistonController(KNEE_ACT , 0.22, 0.20)
 
         self.pist_helpers = [yaw_pist, pitch_pist, knee_pist]
-        
-
-        #self.points.append( (1.6,  0.00, -1.68, 0.2) )
-
-        #points.append( (1.6,  0.00, -1.7) )
-        #self.points.append( (1.7, 0.0, -0.65) )
-        #self.points.append( (1.7, 0.0, -1.58) )
-        #self.points.append( (2.5, 0.0, -1.38) )
-        #self.points.append( (1.2, 0.0, -1.38) )
-
-        # Hackery for notch filter
-        self.hip_yaw_commands   = [0.0 for i in range(int((0.5/2.00) / (1/200.0)))]
-        self.hip_pitch_commands = [0.0 for i in range(int((0.5/6.30) / (1/200.0)))]
-        self.knee_commands = [0.0 for i in range(int((0.5/6.30) / (1/200.0)))]
 
     def update( self, time, yaw, hip_pitch, knee_pitch, shock_depth, command=None):
         # Update model
@@ -100,30 +105,15 @@ class Gait(object):
             self.path = TrapezoidalFootMove(self.model, controller,
                     array(self.points[self.index][0:3]),
                                            self.points[self.index][3], 0.3)
-            # HIP PITCH TUNING DONE AT .20 SPEED
             self.index = (self.index+1)%len(self.points)
 
         # Evaluate path and joint control
-        controller.update(self.model.getJointAngles(), self.path.update())
+        present_angles = self.model.getJointAngles()
+        target_angles  = self.path.update()
+        controller.update(present_angles, target_angles)
 
-        # Send commands
-        rates = controller.getLengthRateCommands()
-
-        commands = [self.pist_helpers[i].update(rates[i]) for i in range(3)]
-
-        # Manual Hackery
-        # Install 2.00Hz notch filter on the hip pitch command to avoid that oscillation we seem to excite on retraction
-        filtered_cmd = (commands[0]+self.hip_yaw_commands.pop(0))/2
-        self.hip_yaw_commands.append(commands[0])
-        commands[0] = filtered_cmd
-        # Install 6.2Hz notch filter on the hip pitch command to avoid that oscillation we seem to excite on retraction
-        filtered_cmd = (commands[1]+self.hip_pitch_commands.pop(0))/2
-        self.hip_pitch_commands.append(commands[1])
-        commands[1] = filtered_cmd
-        # Install 6.2Hz notch filter on the hip pitch command to avoid that oscillation we seem to excite on retraction
-        filtered_cmd = (commands[2]+self.knee_commands.pop(0))/2
-        self.knee_commands.append(commands[2])
-        commands[2] = filtered_cmd
+        ang_rates = [self.joint_controllers[i].update(target_angles[i], present_angles[i]) for i in range(3)]
+        commands  = [self.pist_helpers[i].update(ang_rates[i]) for i in range(3)]
 
         return commands
 
