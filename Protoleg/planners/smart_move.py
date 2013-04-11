efa4a393d8f923fe1157719d9b22d18238f85baf
James Whong <james.whong@gmail.com>
2013-04-11 15:22:36 -0400

    Modified to use LearningPistonControllers.  Added debug messages

--- Protoleg/planners/smart_move.py
+++ Protoleg/planners/smart_move.py
@@ -2,7 +2,7 @@ from ControlsKit import time_sources, LegModel, LimbController
 from ControlsKit.leg_paths import Pause, TrapezoidalFootMove
 from ControlsKit.math_utils import array, Z
 from ControlsKit.time_sources import global_time
-from  joint_controller import PistonController, JointController
+from  joint_controller import PistonController,LearningPistonController, JointController
 from Utilities.ActuatorCharacteristics import *
 
 # Convenience multipliers...
@@ -21,10 +21,16 @@ class Gait(object):
         self.index = 0
         self.points = []
         #self.points.append( (0.4,  0, -0.45, 0.2) )
-        self.points.append( (1.8, -1.00, -0.75, 0.20) )
-        self.points.append( (1.8, -1.00, -1.68, 0.20) )
-        self.points.append( (1.8,  1.00, -1.68, 0.20) )
-        self.points.append( (1.8,  1.00, -0.75, 0.20) )
+
+        #self.points.append( (1.8, -1.00, -0.75, 0.40) )
+        #self.points.append( (1.8, -1.00, -1.68, 0.20) )
+        #self.points.append( (1.8,  1.00, -1.68, 0.40) )
+        #self.points.append( (1.8,  1.00, -0.75, 0.20) )
+
+        #self.points.append( (1.8, -1.00, -1.45, 0.40) )
+        self.points.append( (1.8, -1.00, -1.68, 0.70) )
+        self.points.append( (1.8,  1.00, -1.68, 0.70) )
+        self.points.append( (1.8,  0.00, -1.35, 0.70) )
 
         # Describe the actuator placements at each joint
         YAW_ACT    = ActuatorMathHelper(
@@ -62,32 +68,36 @@ class Gait(object):
         self.act_helpers = [YAW_ACT, PITCH_ACT, KNEE_ACT]
        
         yaw_joint  = JointController(YAW_ACT,
-                kp=1.0,
+                kp=1.5,
                 ki=0.0,
                 kd=0.0,
                 kff=1.0,
                 kfa=0.0,
-                derivative_corner=10)
+                derivative_corner=10,
+                backlash=deg2rad*1.5)
         pitch_joint  = JointController(PITCH_ACT,
-                kp=3.5,
+                kp=2.0,
                 ki=0.0,
                 kd=0.0,
                 kff=1.0,
                 kfa=0.0,
-                derivative_corner=10)
+                derivative_corner=10,
+                backlash=deg2rad*0.5)
         knee_joint  = JointController(KNEE_ACT,
-                kp=3.5,
+                kp=2.0,
                 ki=0.0,
                 kd=0.0,
                 kff=1.0,
                 kfa=0.0,
-                derivative_corner=10)
+                derivative_corner=10,
+                backlash=deg2rad*0.5)
 
         self.joint_controllers = [yaw_joint, pitch_joint, knee_joint]
 
-        yaw_pist   = PistonController(YAW_ACT  , 0.26, 0.26)
-        pitch_pist = PistonController(PITCH_ACT, 0.27, 0.27)
-        knee_pist  = PistonController(KNEE_ACT , 0.22, 0.20)
+        #yaw_pist   = LearningPistonController(YAW_ACT  , 0.26, 0.28)
+        yaw_pist   = LearningPistonController('YAWPIST',   YAW_ACT  , 0.37, 0.37)
+        pitch_pist = LearningPistonController('PITCHPIST', PITCH_ACT, 0.32, 0.32)
+        knee_pist  = LearningPistonController('KNEEPIST',  KNEE_ACT , 0.22, 0.20)
 
         self.pist_helpers = [yaw_pist, pitch_pist, knee_pist]
 
@@ -104,7 +114,7 @@ class Gait(object):
         if self.path.isDone():
             self.path = TrapezoidalFootMove(self.model, controller,
                     array(self.points[self.index][0:3]),
-                                           self.points[self.index][3], 0.3)
+                    self.points[self.index][3], 0.4)
             self.index = (self.index+1)%len(self.points)
 
         # Evaluate path and joint control
@@ -112,12 +122,41 @@ class Gait(object):
         target_angles  = self.path.update()
         controller.update(present_angles, target_angles)
 
-        ang_rates = [self.joint_controllers[i].update(target_angles[i], present_angles[i]) for i in range(3)]
-        commands  = [self.pist_helpers[i].update(ang_rates[i]) for i in range(3)]
+        control_lin_rates = [self.joint_controllers[i].update(target_angles[i], present_angles[i]) for i in range(3)]
+        target_lin_rates = [c.getDesiredLengthRate() for c in self.joint_controllers]
+        measured_lin_rates = [c.getLengthRate() for c in self.joint_controllers]
+
+        commands  = [self.pist_helpers[i].update(control_lin_rates[i], measured_lin_rates[i]) for i in range(3)]
+        #commands  = [0,self.pist_helpers[1].update(control_lin_rates[1], measured_lin_rates[1]),0]
+
+        if 0:
+            target_ang_rates = [c.desired_vel for c in self.joint_controllers]
+            measured_ang_rates = [c.getAngleRate() for c in self.joint_controllers]
+            s = 'Measured: '
+            for m in measured_ang_rates:
+                s += '%1.3f '%(m*180/pi)
+            print s
+            s = 'Target:   '
+            for m in target_ang_rates:
+                s += '%1.3f '%(m*180/pi)
+            print s
+            s = 'M Lin'
+            for m in measured_lin_rates:
+                s += '%1.3f '%m
+            print s
+            s = 'T Lin'
+            for m in target_lin_rates:
+                s += '%1.3f '%m
+            print s
+            self.pist_helpers[0].clay_pit_pos.printpit()
 
         return commands
+    def gracefulExit(self):
+        for i in range(3):
+            self.pist_helpers[i].saveState()
 
 gait = Gait()
 update = gait.update
+graceful_exit = gait.gracefulExit
 controller = gait.controller
 
