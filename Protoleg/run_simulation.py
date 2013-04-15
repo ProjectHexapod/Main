a3f2360da168f6ca89437d2fdf69f6808c4413c3
James Whong <james.whong@gmail.com>
2013-04-14 20:04:53 -0400

    Editted variable names for clarity.  Added callback to gracefully close controllers.

--- Protoleg/run_simulation.py
+++ Protoleg/run_simulation.py
@@ -19,7 +19,10 @@ signal.signal(signal.SIGINT, keyboardInterruptHandler)
 retval = importPlanner()
 # FIXME: This is to allow backwards compatibility to older versions of importPlanner
 if type(retval) == tuple:
-    update, controller = retval
+    if len(retval)==3:
+        update, controller, graceful_exit = retval
+    elif len(retval)==2:
+        update, controller = retval
 else:
     update = retval
     controller = None
@@ -27,15 +30,16 @@ else:
 commands = [0,0,0]
 
 publisher = Publisher(5055)
-publisher.addToCatalog( 'yaw_ang_target'   , controller.getDesiredYawDeg )
-publisher.addToCatalog( 'pitch_ang_target' , controller.getDesiredPitchDeg )
-publisher.addToCatalog( 'knee_ang_target'  , controller.getDesiredKneeDeg )
+if controller != None:
+    publisher.addToCatalog( 'yaw_ang_target'   , controller.getDesiredYawDeg )
+    publisher.addToCatalog( 'pitch_ang_target' , controller.getDesiredPitchDeg )
+    publisher.addToCatalog( 'knee_ang_target'  , controller.getDesiredKneeDeg )
 publisher.addToCatalog( 'yaw_cmd'          , lambda: commands[0] )
 publisher.addToCatalog( 'pitch_cmd'        , lambda: commands[1] )
 publisher.addToCatalog( 'knee_cmd'         , lambda: commands[2] )
 
 d = {'offset':(0,0,2.00)}
-s = Simulator(dt=1e-3,plane=1,pave=0,graphical=1,robot=LegOnColumn,robot_kwargs=d, start_paused = 1, render_objs=1, draw_contacts=1)
+s = Simulator(dt=5e-3,plane=1,pave=0,graphical=1,robot=LegOnColumn,robot_kwargs=d, start_paused = 1, render_objs=1, draw_contacts=1, publish_int=20)
 
 yaw_joint      = s.robot.joints['hip_yaw']
 pitch_joint    = s.robot.joints['hip_pitch']
@@ -51,22 +55,16 @@ last_time = 0.0
 while True:
     s.step()
     time = s.getSimTime()
-    if not s.getPaused() and time - last_time > control_period:
-        lr = update(time,\
+    if not s.getPaused() and time - last_time >= control_period:
+        valve_cmd = update(time,\
                 yaw_joint.getAngle()+yaw_joint_bl.getAngle(),\
                 pitch_joint.getAngle()+pitch_joint_bl.getAngle(),\
                 knee_joint.getAngle()+knee_joint_bl.getAngle(),\
                 shock_joint.getPosition())
         last_time = time
-        commands[0] = lr[0]
-        commands[1] = lr[1]
-        commands[2] = lr[2]
-        #yaw_joint.setLengthRate(lr[0])
-        yaw_joint.setValveCommand(lr[0])
-        #pitch_joint.setLengthRate(lr[1])
-        pitch_joint.setValveCommand(lr[1])
-        #knee_joint.setLengthRate(lr[2])
-        knee_joint.setValveCommand(lr[2])
-        #yaw_joint.setValveCommand(0.5)
-        #pitch_joint.setValveCommand(1.0)
-        #knee_joint.setValveCommand(0.0)
+        commands[0] = valve_cmd[0]
+        commands[1] = valve_cmd[1]
+        commands[2] = valve_cmd[2]
+        yaw_joint.setValveCommand(valve_cmd[0])
+        pitch_joint.setValveCommand(valve_cmd[1])
+        knee_joint.setValveCommand(valve_cmd[2])
