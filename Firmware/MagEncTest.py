4d87a534c51338928de8780b7c6ebaa545b8d216
Ian Katz <ifreecarve@gmail.com>
2013-04-16 15:41:11 -0400

    pep8 conversion

--- Firmware/MagEncTest.py
+++ Firmware/MagEncTest.py
@@ -4,8 +4,8 @@ from math import *
 from array import array
 
 s = serial.Serial(port="COM3", baudrate=1000000, timeout=2e-2)
-#s.open()
-#for c in 'a123456':
+# s.open()
+# for c in 'a123456':
 #	s.write(c)
 cmd = array('B')
 cmd.append(0x40)
@@ -13,19 +13,19 @@ cmd.append(0x00)
 cmd.append(0x01)
 cmd.append(0x02)
 cmd.append(0x03)
-#cmd.append(0x00)
+# cmd.append(0x00)
 n_clear = 0
 
 sin_val = 0
 cos_val = 0
 
-#for i in f:
+# for i in f:
 i = 0
 while 1:
-    #cmd[2] = i
+    # cmd[2] = i
     cmd_string = cmd.tostring()
     s.write(cmd_string)
-    #i = (i + 1)%64
+    # i = (i + 1)%64
     retval = s.read(len(cmd_string))
 
     if len(retval) < 5:
@@ -34,7 +34,7 @@ while 1:
     sin_val = ord(retval[1]) * 256 + ord(retval[2]) - 1207
     cos_val = ord(retval[3]) * 256 + ord(retval[4]) - 1207
                 
-    #for c in retval:
+    # for c in retval:
     for j in range(len(retval)):
         print '%.2x' % ord(retval[j])
         print sin_val, cos_val, atan2(sin_val, cos_val)
--- Firmware/pwm-3cycle.py
+++ Firmware/pwm-3cycle.py
@@ -6,7 +6,7 @@ s = serial.Serial(port="/dev/ttyUSB0", baudrate=500000, timeout=2e-1)
 
 cmd = array('B')
 cmd.append(0x00)
-clrstr = cmd.tostring()*16
+clrstr = cmd.tostring() * 16
 s.write(clrstr)
 retval = s.read(len(clrstr))
 for c in retval:
@@ -20,13 +20,13 @@ cmd.append(0x00)
 cmd.append(0x00)
 
 while True:
-    cmd[0] = 0x03|(((cmd[0]&0xf0)+0x10)%0x30)
-    cmd[2] = (cmd[2]+85)%256
-    cmd[3] = (cmd[3]+85)%256
+    cmd[0] = 0x03 | (((cmd[0] & 0xf0) + 0x10) % 0x30)
+    cmd[2] = (cmd[2] + 85) % 256
+    cmd[3] = (cmd[3] + 85) % 256
     cmdstr = cmd.tostring()
     s.write(cmdstr)
     retval = s.read(len(cmdstr))
     for c in retval:
         print '%.2x' % ord(c),
     print ''
-    sleep(0.01/3)
+    sleep(0.01 / 3)
