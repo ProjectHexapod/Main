aac3169b0483c092b6d196fabb8d8690e3106158
Ian Katz <ifreecarve@gmail.com>
2013-04-16 16:36:56 -0400

    style fixes in Utilities for pep8

--- Utilities/ActuatorCharacteristics.py
+++ Utilities/ActuatorCharacteristics.py
@@ -2,11 +2,12 @@ from math import *
 from SimulationKit.helpers import *
 
 # Convenience multipliers...
-deg2rad    = pi/180
+deg2rad    = pi / 180
 psi2pascal = 6894.76
 inch2meter = 2.54e-2
 pound2kilo = 0.455
-gallon2cmps = 1/15850.4
+gallon2cmps = 1 / 15850.4
+
 
 class ActuatorCharacteristics(object):
     """
@@ -27,119 +28,143 @@ class ActuatorCharacteristics(object):
         self.AXIS                   = axis
         self.ANG_OFFSET             = ang_offset
         self.SYSTEM_PRESSURE        = system_pressure
-    def getRangeOfMotion( self ):
+
+    def getRangeOfMotion(self):
         """Returns an angle in radians"""
-        assert abs(len2(self.PIVOT2)-self.PIVOT1_DIST_FROM_JOINT) <\
+        assert abs(len2(self.PIVOT2) - self.PIVOT1_DIST_FROM_JOINT) <\
             self.ACT_RETRACTED_LEN, 'You can retract in to singularity'
-        retracted_angle = thetaFromABC(\
-            self.PIVOT1_DIST_FROM_JOINT,\
-            len2(self.PIVOT2),\
+        retracted_angle = thetaFromABC(
+            self.PIVOT1_DIST_FROM_JOINT,
+            len2(self.PIVOT2),
             self.ACT_RETRACTED_LEN)
-        assert len2(self.PIVOT2)+self.PIVOT1_DIST_FROM_JOINT >\
+        assert len2(self.PIVOT2) + self.PIVOT1_DIST_FROM_JOINT >\
             self.ACT_EXTENDED_LEN, 'You can extend in to singularity'
-        extended_angle = thetaFromABC(\
-            self.PIVOT1_DIST_FROM_JOINT,\
-            len2(self.PIVOT2),\
+        extended_angle = thetaFromABC(
+            self.PIVOT1_DIST_FROM_JOINT,
+            len2(self.PIVOT2),
             self.ACT_EXTENDED_LEN)
-        assert extended_angle>retracted_angle, \
+        assert extended_angle > retracted_angle, \
             "This actuator placement breaks sign conventions\
             ... actuator extend should increase joint angle"
         return extended_angle - retracted_angle
-    def getExtensionCrossSectionM2( self ):
+
+    def getExtensionCrossSectionM2(self):
         """Returns piston cross sectional area in m^2 on the extend side of the
         piston"""
-        return ((self.BORE_DIAMETER/2)**2)*pi
-    def getRetractionCrossSectionM2( self ):
+        return ((self.BORE_DIAMETER / 2) ** 2) * pi
+
+    def getRetractionCrossSectionM2(self):
         """Returns piston cross sectional area in m^2 on the extend side of the
         piston"""
-        return ((self.BORE_DIAMETER/2)**2-(self.ROD_DIAMETER/2)**2)*pi
-    def getMaxExtensionForceNewtons( self ):
+        return ((self.BORE_DIAMETER / 2) ** 2 - (self.ROD_DIAMETER / 2) ** 2) * pi
+
+    def getMaxExtensionForceNewtons(self):
         """Pressure is in Newtons/sq. meter.  Returned force is in Newtons"""
-        return self.getExtensionCrossSectionM2()*self.SYSTEM_PRESSURE
-    def getMaxRetractionForceNewtons( self, pressure = 0.0 ):
+        return self.getExtensionCrossSectionM2() * self.SYSTEM_PRESSURE
+
+    def getMaxRetractionForceNewtons(self, pressure=0.0):
         """Pressure is in Newtons/sq. meter.  Returned force is in Newtons"""
-        return self.getRetractionCrossSectionM2()*self.SYSTEM_PRESSURE
+        return self.getRetractionCrossSectionM2() * self.SYSTEM_PRESSURE
 
 
 class ActuatorMathHelper(ActuatorCharacteristics):
     def __init__(self, *args, **kwargs):
         super(ActuatorMathHelper, self).__init__(*args, **kwargs)
-        self.setActuatorAnchors( self.PIVOT1_DIST_FROM_JOINT, self.PIVOT2[0], self.PIVOT2[1] )
-        self.setExtendCrossSection(  pi *  (self.BORE_DIAMETER/2)**2 )
-        self.setRetractCrossSection( pi * ((self.BORE_DIAMETER/2)**2 - (self.ROD_DIAMETER/2)**2) )
-    def setActuatorAnchors( self, a1_x, a2_x, a2_y ):
+        self.setActuatorAnchors(self.PIVOT1_DIST_FROM_JOINT, self.PIVOT2[0], self.PIVOT2[1])
+        self.setExtendCrossSection(pi * (self.BORE_DIAMETER / 2) ** 2)
+        self.setRetractCrossSection(pi * ((self.BORE_DIAMETER / 2) ** 2 - (self.ROD_DIAMETER / 2) ** 2))
+
+    def setActuatorAnchors(self, a1_x, a2_x, a2_y):
         self.a1_x = a1_x
         self.a2_x = a2_x
         self.a2_y = a2_y
-    def setAngle( self, ang ):
+
+    def setAngle(self, ang):
         """
         Set the angle from which we are calculating our lengths
         """
         self.ang_from_max_retraction = ang - self.ANG_OFFSET
-    def setAngleRate( self, rate ):
+
+    def setAngleRate(self, rate):
         self.ang_rate = rate
-    def getLength( self ):
+
+    def getLength(self):
         a = self.getActPath()
         return len2(a)
-    def getLengthRate( self ):
+
+    def getLengthRate(self):
         arate = self.ang_rate
         larm  = self.getLeverArm()
-        return arate*larm
-    def setCrossSection( self, area ):
+        return arate * larm
+
+    def setCrossSection(self, area):
         self.setExtendCrossSection(area)
         self.setRetractCrossSection(area)
-    def setMaxHydraulicFlow( self, new_max ):
-        self.max_extend_rate  = new_max/self.extend_cross_section
-        self.max_retract_rate = -new_max/self.retract_cross_section
-    def setExtendCrossSection( self, area ):
+
+    def setMaxHydraulicFlow(self, new_max):
+        self.max_extend_rate  = new_max / self.extend_cross_section
+        self.max_retract_rate = -new_max / self.retract_cross_section
+
+    def setExtendCrossSection(self, area):
         self.extend_cross_section = area
-    def setRetractCrossSection( self, area ):
+
+    def setRetractCrossSection(self, area):
         self.retract_cross_section = area
-    def getActPath( self ):
+
+    def getActPath(self):
         """
         Return the path of the actuator in the hinge plane
         """
         # Apply joint rotation to one anchor
         ang = self.ang_from_max_retraction
         # Get the angle of the actuator with the horizontal
-        act = rot2( (self.a2_x, self.a2_y), ang )
+        act = rot2((self.a2_x, self.a2_y), ang)
         act = (act[0] - self.a1_x, act[1])
         return act
-    def getLeverArm( self ):
+
+    def getLeverArm(self):
         act = self.getActPath()
-        h_ang = pi-atan2(act[1], act[0])
+        h_ang = pi - atan2(act[1], act[0])
         # Get the lever arm
-        l_arm = sin(h_ang)*self.a1_x
+        l_arm = sin(h_ang) * self.a1_x
         return l_arm
-    def __force_to_torque( self, force ):
+
+    def __force_to_torque(self, force):
         """
         Give the max applicable torque at the present orientation
         given an actuator force
         """
-        return self.getLeverArm()*force
-    def __torque_to_force( self, torque ):
-        return torque/self.getLeverArm()
-    def setForceLimit( self, f ):
-        self.setExtendForceLimit( f )
-        self.setRetractForceLimit( f )
-    def setExtendForceLimit( self, f ):
+        return self.getLeverArm() * force
+
+    def __torque_to_force(self, torque):
+        return torque / self.getLeverArm()
+
+    def setForceLimit(self, f):
+        self.setExtendForceLimit(f)
+        self.setRetractForceLimit(f)
+
+    def setExtendForceLimit(self, f):
         self.extend_force_limit = f
-    def getExtendForceLimit( self ):
+
+    def getExtendForceLimit(self):
         return self.extend_force_limit
-    def getRetractForceLimit( self ):
+
+    def getRetractForceLimit(self):
         return self.retract_force_limit
-    def setRetractForceLimit( self, f ):
+
+    def setRetractForceLimit(self, f):
         self.retract_force_limit = f
-    def setTorqueLimit( self, l ):
+
+    def setTorqueLimit(self, l):
         print 'Cannot set torque limit on linear actuator controlled hinge'
-    def getTorqueLimit( self ):
+
+    def getTorqueLimit(self):
         """The torque limit is directional depending on what side of the piston
         is being driven.  This is dependent on which way we are pressurizing the
         piston, not which way it's presently moving"""
-        #FIXME: THIS ASSUMES WE ARE USING THE JOINT'S ANG_TARGET PARAMETER,
+        # FIXME: THIS ASSUMES WE ARE USING THE JOINT'S ANG_TARGET PARAMETER,
         # WHICH WE OFTEN ARE NOT
-        if self.getAngleError()>0:
-            return abs(self.getLeverArm()*self.extend_force_limit)
+        if self.getAngleError() > 0:
+            return abs(self.getLeverArm() * self.extend_force_limit)
         else:
-            return abs(self.getLeverArm()*self.retract_force_limit)
-
+            return abs(self.getLeverArm() * self.retract_force_limit)
--- Utilities/pubsub.py
+++ Utilities/pubsub.py
@@ -4,20 +4,23 @@ import threading
 import StringIO
 import time
 
+
 class Publisher(object):
     _instance = None
+
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             print 'Creating new Publisher'
-            cls._instance = super(Publisher, cls).__new__(
-                                cls, *args, **kwargs)
+            cls._instance = super(Publisher, cls).__new__(cls, *args, **kwargs)
+            
         return cls._instance
-    def __init__( self, port ):
+
+    def __init__(self, port):
         if hasattr(self, 'initialized'):
             print 'Publisher has already initialized, but new one requested.  Returning old instance'
             return
         else:
-            print "Creating new Publisher on port %d"%port
+            print "Creating new Publisher on port %d" % port
         # Start the publishing server on specified TCP port
         self.port         = port
         self.sock         = None
@@ -35,24 +38,24 @@ class Publisher(object):
         # Flag to let the world know we're initialized
         self.initialized = 1
 
-    def addToCatalog( self, name, callback ):
+    def addToCatalog(self, name, callback):
         self.catalog[name] = callback
 
-    def start( self ):
+    def start(self):
         if not self.serverThread:
             self.serverThread     = threading.Thread()
             self.serverThread.run = self.run_server
-            self.serverThread.setDaemon( True )
+            self.serverThread.setDaemon(True)
             self.serverThread.start()
     
-    def publish( self ):
+    def publish(self):
         # Trigger the publisher to send a frame of data
         if self.ready_to_publish and len(self.subscriptions):
             try:
                 frame = {}
                 for k in self.subscriptions:
                     f = self.catalog[k]
-                    if type(f)==tuple:
+                    if type(f) == tuple:
                         frame[k] = f[0](f[1])
                     else:
                         frame[k] = f()
@@ -65,16 +68,16 @@ class Publisher(object):
         for publisher in self.sub_publishers:
             publisher.publish()
 
-    def run_server( self ):
-        print 'Hello from the publisher server thread on port %d'%self.port
-        self.sock = socket( AF_INET, SOCK_STREAM )
+    def run_server(self):
+        print 'Hello from the publisher server thread on port %d' % self.port
+        self.sock = socket(AF_INET, SOCK_STREAM)
         addr = ('localhost', self.port)
         # This is a hack to allow fast restarts
         self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self.sock.bind( (addr) )
+        self.sock.bind((addr))
         while 1:
             self.sock.listen(1)
-            self.conn,self.remote_addr = self.sock.accept()
+            self.conn, self.remote_addr = self.sock.accept()
             # Send a copy of the catalog
             s = pickle.dumps(self.catalog.keys())
             self.conn.send(s)
@@ -92,21 +95,21 @@ class Publisher(object):
                     print mesg
                     self.restartflag = 1
                 if s:
-                    #try:
+                    # try:
                     data = pickle.loads(s)
-                    if type(data) == type(0):
+                    if isinstance(data, (int)):
                         # Request to open another publisher on a different
                         # port.
                         port = data
                         print "Received request for new publisher on port"\
-                            " %d"%port
+                            " %d" % port
                         new_pub = subPublisher(port)
                         # FIXME: This feels hacky... new publisher refers to
                         # the old publisher's catalog
                         new_pub.catalog = self.catalog
                         new_pub.start()
                         self.sub_publishers.append(new_pub)
-                    elif type(data) == type([]):
+                    elif isinstance(data, (List)):
                         # Subscription request
                         keys = data
                         self.subscriptions = keys
@@ -114,7 +117,7 @@ class Publisher(object):
                     else:
                         print "Received unhandled request!"
                         print data
-                    #except e, msg:
+                    # except e, msg:
                     #    print 'Pickle fail'
                     #    self.restartflag = 1
                 else:
@@ -124,12 +127,13 @@ class Publisher(object):
             self.conn.close()
             self.conn = None
 
-    def close( self ):
+    def close(self):
         print 'Closing publisher gracefully'
         self.conn.close()
         self.sock.close()
         for publisher in self.sub_publishers:
             publisher.close()
+
 
 class subPublisher(Publisher):
     """
@@ -148,42 +152,46 @@ class Subscriber:
         self.stop_flag = 0
         self.frameReceivedCallback = None
         self.clientThread = None
-        self.sock = socket( AF_INET, SOCK_STREAM )
+        self.sock = socket(AF_INET, SOCK_STREAM)
         self.sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         # Call this when the other side disconnects
         self.remoteDisconnectCallback = None
         while 1:
             try:
-                self.sock.connect( (host,port) )
+                self.sock.connect((host, port))
             except error, desc:
                 print desc
                 time.sleep(1.0)
             try:
-                #waiting for catalog
+                # waiting for catalog
                 self.catalog = pickle.loads(self.sock.recv(4096))
                 break
             except error, desc:
                 print desc
                 time.sleep(1.0)
         self.catalog.sort()
-    def start( self ):
+
+    def start(self):
         self.stop_flag = 0
         self.clientThread = threading.Thread()
         self.clientThread.run = self.run_client
-        self.clientThread.setDaemon( True )
+        self.clientThread.setDaemon(True)
         self.clientThread.start()
-    def subscribeTo( self, name_list ):
-        self.sock.send( pickle.dumps(name_list) )
-    def requestAnotherPublisher( self, port ):
-        print "Requesting another publisher on %d"%port
-        self.sock.send( pickle.dumps(port) )
-    def run_client( self ):
+
+    def subscribeTo(self, name_list):
+        self.sock.send(pickle.dumps(name_list))
+
+    def requestAnotherPublisher(self, port):
+        print "Requesting another publisher on %d" % port
+        self.sock.send(pickle.dumps(port))
+
+    def run_client(self):
         # The client waits to receive frames and then calls the callback
         while not self.stop_flag:
             try:
                 recv_string = self.sock.recv(4096)
                 frame  = pickle.loads(recv_string)
-                self.frameReceivedCallback( frame )
+                self.frameReceivedCallback(frame)
             except error, mesg:
                 # Socket error
                 print mesg
@@ -195,30 +203,32 @@ class Subscriber:
                     self.stop_flag = 1
                     if self.remoteDisconnectCallback:
                         self.remoteDisconnectCallback()
-    def setCallback( self, callback ):
+
+    def setCallback(self, callback):
         self.frameReceivedCallback = callback
-    def close( self ):
+
+    def close(self):
         print 'Closing subscriber gracefully'
-        self.stop_flag =  1
+        self.stop_flag = 1
         self.sock.close()
         print 'Done'
 
-if __name__=='__main__':
-    def dummy_callback( frame ):
+if __name__ == '__main__':
+    def dummy_callback(frame):
         print 'Got frame!'
-        for k,v in frame.items():
-            print k,v
+        for k, v in frame.items():
+            print k, v
     p = Publisher(5055)
     q = Publisher(5057)
     if id(p) != id(q):
         print "Only one publisher should ever be instantiated!"
     import random
-    p.addToCatalog('foo', lambda: random.uniform(0,1))
-    p.addToCatalog('bar', lambda: random.uniform(0,1))
+    p.addToCatalog('foo', lambda: random.uniform(0, 1))
+    p.addToCatalog('bar', lambda: random.uniform(0, 1))
     p.start()
     time.sleep(0.1)
-    s = Subscriber( 'localhost', 5055 )
-    s.setCallback( dummy_callback )
+    s = Subscriber('localhost', 5055)
+    s.setCallback(dummy_callback)
     s.subscribeTo(['foo'])
     s.start()
     for x in range(20):
